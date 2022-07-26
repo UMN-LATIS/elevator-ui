d21a477183b656ec402a8f654d389038785febed
Colin McFadden <cmcfadden@gmail.com>
2022-07-26 14:46:18 -0500

    finish up storybook for related items

--- src/ViewComponents/RelatedAssetWidget/RelatedAssetWidget.stories.js
+++ src/ViewComponents/RelatedAssetWidget/RelatedAssetWidget.stories.js
@@ -31,10 +31,44 @@ let field = "relatedstuff_1"
 let widgetContents = mockAsset[field];
 let template = getField(mockTemplate, field);
 
-export const Default = Template.bind({});
-Default.args = {
+let collapseItem = JSON.parse(JSON.stringify(template));
+collapseItem.fieldData.collapseNestedChildren = true;
+export const CollapsedInlineRelatedAsset = Template.bind({});
+CollapsedInlineRelatedAsset.args = {
     contents: widgetContents,
-    widget: template,
+    widget: collapseItem,
+    asset: mockAsset
+};
+
+let templateThumb = JSON.parse(JSON.stringify(template));
+templateThumb.fieldData.collapseNestedChildren = false;
+templateThumb.fieldData.thumbnailView = true;
+export const ThumbnailView = Template.bind({});
+ThumbnailView.args = {
+    contents: widgetContents,
+    widget: templateThumb,
+    asset: mockAsset
+};
+
+
+let linkedItem = JSON.parse(JSON.stringify(template));
+linkedItem.fieldData.nestData = false;
+linkedItem.fieldData.collapseNestedChildren = false;
+export const LinkedItemRelatedAsset = Template.bind({});
+LinkedItemRelatedAsset.args = {
+    contents: widgetContents,
+    widget: linkedItem,
+    asset: mockAsset
+};
+
+let togglePanel = JSON.parse(JSON.stringify(template));
+togglePanel.fieldData.nestData = true;
+togglePanel.fieldData.collapseNestedChildren = false;
+togglePanel.fieldData.thumbnailView = false;
+export const TogglePanelRelatedAsset = Template.bind({});
+TogglePanelRelatedAsset.args = {
+    contents: widgetContents,
+    widget: togglePanel,
     asset: mockAsset
 };
 
