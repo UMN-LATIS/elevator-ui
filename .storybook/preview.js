f6089df24bd828d6c47b1dc1d5d1e37f050dd6e6
James Johnson <johnsojr@umn.edu>
2022-07-26 17:13:59 -0500

    space

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import "../src/app.css";
 import "@fontsource/material-icons";
 window.baseURL = "https://dev.elevator.umn.edu/defaultinstance/";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
