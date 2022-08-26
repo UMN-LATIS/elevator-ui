664f354c82826c16d6da845a3fafd15b4500a5a1
James Johnson <johnsojr@umn.edu>
2022-08-26 10:09:22 -0500

    Chip Stories

--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -2,7 +2,11 @@
   <component
     :is="href ? 'a' : 'span'"
     :href="href"
-    class="inline-flex items-center px-2.5 py-0.5 rounded-md text-xs font-medium bg-transparent-black-100 no-underline hover:underline text-neutral-700"
+    class="inline-flex items-center px-2.5 py-0.5 rounded-md text-xs font-medium bg-transparent-black-100 hover:no-underline no-underline text-neutral-700"
+    :class="{
+      'border border-neutral-300 cursor-pointer hover:border-neutral-600 hover:bg-neutral-200 transition-colors ease-in-out':
+        href,
+    }"
   >
     <slot />
   </component>
