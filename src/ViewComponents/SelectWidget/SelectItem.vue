91e9079899aba4c116901c1387b14fb1c5a911ef
James Johnson <johnsojr@umn.edu>
2022-08-30 18:29:00 -0500

    disable getDisplayText for now

--- src/ViewComponents/SelectWidget/SelectItem.vue
+++ src/ViewComponents/SelectWidget/SelectItem.vue
@@ -1,10 +1,7 @@
 <template>
   <li v-for="(content, key) in contentAsArray" :key="key">
-    <Link
-      :widget="widget"
-      :linkText="getDisplayText(content)"
-      v-html="getDisplayText(content)"
-    >
+    <Link :widget="widget" :linkText="content">
+      <div v-html="content" />
     </Link>
   </li>
 </template>
@@ -12,15 +9,12 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import Link from "@/Helpers/Link/Link.vue";
-import { Widget } from "@/types";
-import { WidgetContents } from "@/types";
+import { SelectWidget, SelectWidgetContents } from "@/types";
 
-interface Props {
-  selectValues: WidgetContents;
-  widget: Widget;
-}
-
-const props = defineProps<Props>();
+const props = defineProps<{
+  selectValues: SelectWidgetContents;
+  widget: SelectWidget;
+}>();
 
 const contentAsArray = computed(() => {
   if (typeof props.selectValues.fieldContents === "string") {
@@ -30,11 +24,12 @@ const contentAsArray = computed(() => {
   }
 });
 
-function getDisplayText(content: string): string {
-  if (props.widget.fieldData.selectGroup.hasOwnProperty(content)) {
-    return props.widget.fieldData.selectGroup[content];
-  } else {
-    return content;
-  }
-}
+// FIXME: selectGroup is possibly undefined. What's the intent here?
+// function getDisplayText(content: string): string {
+//   if (props.widget.fieldData.selectGroup.hasOwnProperty(content)) {
+//     return props.widget.fieldData?.selectGroup[content];
+//   } else {
+//     return content;
+//   }
+// }
 </script>
