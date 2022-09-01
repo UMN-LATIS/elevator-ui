b3cc98ab84fb133d9071aeea1aa6d4df0db7792e
James Johnson <johnsojr@umn.edu>
2022-09-01 11:19:27 -0500

    Object Viewer

---
+++ src/components/ObjectViewer.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="object-viewer">
+    <h2 class="sr-only">Object Viewer</h2>
+    <iframe
+      v-if="embedLink"
+      :src="embedLink"
+      :style="{ width: '100%', height: '100%' }"
+      frameBorder="0"
+      allowfullscreen="true"
+    ></iframe>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+import config from "@/config";
+import { Asset } from "@/types";
+import { useAssetStore } from "@/stores/newAssetStore";
+
+const props = defineProps<{
+  objectId: string;
+}>();
+
+// this is the object that will be viewed
+// (of type asset)
+const objectAsset = ref<Asset | null>(null);
+const fileObjectId = computed(
+  () => objectAsset.value?.firstFileHandlerId ?? null
+);
+
+const embedLink = computed((): null | string => {
+  if (!props.objectId || !fileObjectId.value) {
+    return null;
+  }
+
+  return `${config.baseUrl}/asset/getEmbed/${fileObjectId.value}/${props.objectId}`;
+});
+
+const assetStore = useAssetStore();
+
+onMounted(async () => {
+  objectAsset.value = await assetStore.fetchAsset(props.objectId);
+});
+</script>
+<style scoped>
+.object-viewer {
+  background: #ddd;
+}
+</style>
