f1c2eed2aa1d38046399ee7120a9ca098c2c102a
James Johnson <johnsojr@umn.edu>
2022-09-23 11:43:10 -0500

    upload widget

--- src/components/Widget/UploadWidget/UploadWidget.vue
+++ src/components/Widget/UploadWidget/UploadWidget.vue
@@ -1,24 +1,39 @@
 <!-- todo need to fix all the hover state stuff, decide if that matters-->
 <template>
-  <ul class="imageList">
-    <li v-for="(content, key) in contents" :key="key">
-      <div class="imageContainer">
-        <UploadWidgetItem
-          :content="content"
-          :widget="widget"
-          class="squareItem"
-        />
-      </div>
-    </li>
-  </ul>
+  <div class="upload-widget grid grid-cols-3 gap-1">
+    <button
+      v-for="(content, key) in contents"
+      :key="key"
+      class="aspect-square w-full inline-block overflow-hidden rounded"
+      :class="{
+        ' border-neutral-900 border-2 p-px': isFileActive(content.fileId),
+        'opacity-50 hover:opacity-100 transition-opacity': !isFileActive(
+          content.fileId
+        ),
+      }"
+      @click="assetStore.activeFileObjectId = content.fileId"
+    >
+      <img
+        :src="`${config.baseUrl}/fileManager/getDerivativeById/${content.fileId}/tiny2x`"
+        :alt="content.fileDescription"
+        class="w-full h-full object-cover rounded-sm"
+      />
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { UploadWidgetProps, UploadWidgetContent } from "@/types";
-import UploadWidgetItem from "./UploadWidgetItem.vue";
+import config from "@/config";
+import { useAssetStore } from "@/stores/assetStore";
 
 defineProps<{
   widget: UploadWidgetProps;
   contents: UploadWidgetContent[];
 }>();
+
+const assetStore = useAssetStore();
+
+const isFileActive = (fileId: string) =>
+  assetStore.activeFileObjectId === fileId;
 </script>
