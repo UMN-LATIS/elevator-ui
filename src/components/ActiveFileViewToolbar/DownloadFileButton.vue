f4e4c1aba07411a6fd56cdaabed3739393ef2785
James Johnson <johnsojr@umn.edu>
2022-10-24 14:35:43 -0500

    open modal on download click

--- src/components/ActiveFileViewToolbar/DownloadFileButton.vue
+++ src/components/ActiveFileViewToolbar/DownloadFileButton.vue
@@ -1,42 +1,42 @@
 <template>
-  <Tooltip>
-    <ActiveFileViewButton @click="handleDownloadFileClick">
-      download
-    </ActiveFileViewButton>
-    <template #content>
-      <h2 class="text-sm uppercase text-neutral-400 pb-2 border-b mb-4">
-        File Downloads
-      </h2>
-      <div v-if="isDownloadFileInfoReady">
-        <span v-if="!downloadFileInfo">No Downloads available</span>
-        <ul v-if="downloadFileInfo">
-          <li v-if="downloadFileInfo.screen?.ready">
-            <a
-              :href="`${config.baseUrl}/fileManager/getDerivativeById/${assetStore.activeFileObjectId}/screen`"
-            >
-              Download Derivative ({{
-                getExtensionFromFilename(
-                  downloadFileInfo.screen.originalFilename
-                )
-              }})
-            </a>
-          </li>
-        </ul>
-      </div>
-    </template>
-  </Tooltip>
+  <ActiveFileViewButton @click="handleDownloadFileClick">
+    download
+  </ActiveFileViewButton>
+  <Modal
+    label="File Downloads"
+    :isOpen="isOpen"
+    class="h-fit m-auto"
+    @close="isOpen = false"
+  >
+    <div v-if="isDownloadFileInfoReady">
+      <span v-if="!downloadFileInfo">No Downloads available</span>
+      <ul v-if="downloadFileInfo">
+        <li v-if="downloadFileInfo.screen?.ready">
+          <a
+            :href="`${config.baseUrl}/fileManager/getDerivativeById/${assetStore.activeFileObjectId}/screen`"
+          >
+            Download Derivative ({{
+              getExtensionFromFilename(
+                downloadFileInfo.screen.originalFilename
+              )
+            }})
+          </a>
+        </li>
+      </ul>
+    </div>
+  </Modal>
 </template>
 <script setup lang="ts">
 import { ref, computed } from "vue";
-import Tooltip from "@/components/Tooltip/Tooltip.vue";
 import ActiveFileViewButton from "./ActiveFileViewButton.vue";
 import { useAssetStore } from "@/stores/assetStore";
 import { FileDownloadResponse } from "@/types/FileDownloadTypes";
 import api from "@/helpers/api";
 import config from "@/config";
+import Modal from "../Modal/Modal.vue";
 
 const assetStore = useAssetStore();
-const isShowingDownloadOptions = ref(false);
+const isOpen = ref(false);
 const downloadFileInfo = ref<FileDownloadResponse | null | undefined>(
   undefined
 );
@@ -50,7 +50,7 @@ const getExtensionFromFilename = (filename) => {
 };
 
 async function handleDownloadFileClick() {
-  isShowingDownloadOptions.value = !isShowingDownloadOptions.value;
+  isOpen.value = true;
   downloadFileInfo.value = undefined; // undef means we're fetching
   downloadFileInfo.value = await api.getFileDownloadInfo(
     assetStore.activeFileObjectId,
