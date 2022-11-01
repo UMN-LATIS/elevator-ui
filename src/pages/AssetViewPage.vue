a6e7328ec2b52ddb55b82ad2b8396f57fba6b91f
James Johnson <johnsojr@umn.edu>
2022-11-01 08:49:44 -0500

    change metadataonly and asset views to dynamic components

--- src/pages/AssetViewPage.vue
+++ src/pages/AssetViewPage.vue
@@ -12,12 +12,15 @@
   </DefaultLayout>
 </template>
 <script setup lang="ts">
-import { ref, watch, computed } from "vue";
+import { ref, watch, computed, defineAsyncComponent } from "vue";
 import { useAssetStore } from "@/stores/assetStore";
-import MetaDataOnlyView from "./MetaDataOnlyView.vue";
-import AssetView from "./AssetView.vue";
 import { useRoute } from "vue-router";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
+
+const MetaDataOnlyView = defineAsyncComponent(
+  () => import("./MetaDataOnlyView.vue")
+);
+const AssetView = defineAsyncComponent(() => import("./AssetView.vue"));
 
 const assetStore = useAssetStore();
 const isMetaDataOnly = computed(() => !assetStore.activeFileObjectId);
