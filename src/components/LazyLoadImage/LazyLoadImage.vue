6daec4a117fd6fe22a7b106dbf1812a0000968f7
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2022-11-03 16:09:02 +0000

    show placeholder while loading

--- src/components/LazyLoadImage/LazyLoadImage.vue
+++ src/components/LazyLoadImage/LazyLoadImage.vue
@@ -1,22 +1,21 @@
 <template>
   <div
     ref="imgContainer"
-    class="lazy-load-image relative 'min-w-[4rem] min-h-[4rem]"
-    :class="{
-      'w-full h-full': isLoaded,
-      'w-32 h-32 max-w-full max-h-full': !isLoaded,
-    }"
+    class="lazy-load-image relative w-full h-full min-w-16 min-h-16"
   >
     <img
       v-if="isLoaded"
-      class="lazy-load-image__image block bg-neutral-100"
+      class="lazy-load-image__image block bg-neutral-100 opacity-0 transition-opacity"
+      :class="{
+        'opacity-100': isImageLoadComplete,
+      }"
       :src="src"
       :alt="alt"
       v-bind="$attrs"
       @load="isImageLoadComplete = true"
     />
     <div
-      v-if="!isLoaded"
+      v-if="!isImageLoadComplete"
       class="absolute inset-0 z-10 flex justify-center items-center bg-neutral-200 border border-neutral-300 text-neutral-400"
     >
       <Icon>image</Icon>
@@ -34,13 +33,13 @@ defineProps<{
 
 const observerOptions = {
   // load images when they are `rootMargin` pixels away from the viewport
-  rootMargin: "100px",
+  rootMargin: "640px",
   // load images when they are `threshold` within the viewport
   // (0 when just a tiny tiny bit, 1 when 100%)
   threshold: 0,
 };
 
-const imgContainer = ref<HTMLImageElement | null>(null);
+const imgContainer = ref<HTMLDivElement | null>(null);
 const isLoaded = ref(false);
 const isImageLoadComplete = ref(false);
 
