04eed5b00bc2e66359845ba9b86fbc5e59e22153
James Johnson <johnsojr@umn.edu>
2022-11-03 09:03:21 -0500

    add page for testing lazy loading locally

---
+++ src/pages/TestPages/LazyLoadImageTest.vue
@@ -0,0 +1,19 @@
+<template>
+  <DefaultLayout>
+    <div class="lazy-load-page">
+      <h1 class="text-xl">LazyLoad Test Page</h1>
+      <div v-for="i in 400" :key="i">
+        <LazyLoadImage
+          :src="`https://picsum.photos/seed/${i}/300/300`"
+          :alt="`img ${i}`"
+        />
+      </div>
+    </div>
+  </DefaultLayout>
+</template>
+<script setup lang="ts">
+import LazyLoadImage from "@/components/LazyLoadImage/LazyLoadImage.vue";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+// import { nextTick, onMounted, ref } from "vue";
+</script>
+<style scoped></style>
