ac1e3597bd8cd29cb5c57a87ea0ea6b2a20d7d5a
James Johnson <johnsojr@umn.edu>
2022-11-07 10:18:32 -0600

    fix: replace `MenuAccordion` with `div`
    git blame James! The MenuAccordion component was dropped in a refactor, but exists in this component so it generates TS errors since it can't be found. This removes the import and replaces the old component with a div.

--- src/components/AppHeader/PageMenu.vue
+++ src/components/AppHeader/PageMenu.vue
@@ -2,7 +2,7 @@
   <nav v-if="items.length">
     <ul class="flex gap-4 items-center">
       <li v-for="menuItem in items" :key="menuItem.name">
-        <MenuAccordion v-if="menuItem.children" :label="menuItem.name">
+        <div v-if="menuItem.children" :label="menuItem.name">
           <ul
             class="absolute flex flex-col gap-2 w-48 bg-neutral-50 p-4 shadow rounded top-full mt-2 left-0"
           >
@@ -12,7 +12,7 @@
               :item="child"
             />
           </ul>
-        </MenuAccordion>
+        </div>
         <a v-else :href="menuItem.href" class="text-current">{{
           menuItem.name
         }}</a>
@@ -21,7 +21,6 @@
   </nav>
 </template>
 <script setup lang="ts">
-import MenuAccordion from "./MenuAccordion.vue";
 import ChildMenuItem from "./ChildMenuItem.vue";
 import { MenuItemWithOptionalChildren } from "@/types";
 
