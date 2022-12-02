904dfcc2fd4878837e489492f593bcd4dfa69622
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2022-12-02 01:16:14 +0000

    more colors for avatar

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -32,15 +32,14 @@ const initials = computed(() => {
 // possible avatar color sets
 const colorOptions = [
   "bg-blue-100 text-blue-700 border-blue-200",
-  // "bg-green-100 text-green-700",
-  // "bg-indigo-100 text-indigo-700",
-  // "bg-yellow-100 text-yellow-700",
-  // "bg-red-100 text-red-700",
-  // "bg-cyan-100 text-cyan-700",
-  // "bg-purple-100 text-purple-700",
-  // "bg-orange-100 text-orange-700",
-  // "bg-teal-100 text-teal-700",
-  // "bg-neutral-100 text-neatral-700",
+  "bg-green-100 text-green-700 border-green-200",
+  "bg-indigo-100 text-indigo-700 border-indigo-200",
+  "bg-yellow-100 text-yellow-700 border-yellow-200",
+  "bg-red-100 text-red-700 border-red-200",
+  "bg-cyan-100 text-cyan-700 border-cyan-200",
+  "bg-purple-100 text-purple-700 border-purple-200",
+  "bg-orange-100 text-orange-700 border-orange-200",
+  "bg-teal-100 text-teal-700 border-teal-200",
 ];
 
 const avatarColorClasses = computed(() => {
