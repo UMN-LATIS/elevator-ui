b6de149893131b0cb9a1c033c707694498242b83
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2022-12-05 21:06:08 +0000

    metadata only page layout fix

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen flex flex-col">
     <AppHeader class="fixed top-0 w-full z-20 backdrop-blur-sm md:static" />
-    <div class="flex-1 mt-20 md:mt-0">
+    <div class="flex-1 mt-18 md:mt-0 overflow-auto">
       <slot />
     </div>
   </div>
