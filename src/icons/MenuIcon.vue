b39dcbfb7696f0390b9565a2e068124287574022
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2022-12-07 00:23:41 +0000

    tweak menu icon for slightly better antialiasing... maybe?

--- src/icons/MenuIcon.vue
+++ src/icons/MenuIcon.vue
@@ -3,9 +3,9 @@
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    stroke-width="2"
     stroke="currentColor"
-    class="w-5 h-5"
+    class="w-6 h-6"
   >
     <path
       stroke-linecap="round"
