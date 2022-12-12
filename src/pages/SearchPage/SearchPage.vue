2c4b15c05a7935ab562689f50d0c8bcb1dd77d01
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2022-12-12 21:00:45 +0000

    show loading state

--- src/pages/SearchPage/SearchPage.vue
+++ src/pages/SearchPage/SearchPage.vue
@@ -5,8 +5,12 @@
         Search results for:
         <span class="font-bold text-2xl">{{ searchText }}</span>
       </h2>
-      <p v-if="matches.length === 0">No results found.</p>
-      <div v-else class="search-results">
+      <div v-if="loadStatus === 'loading'">
+        <SpinnerIcon />
+      </div>
+      <p v-if="loadStatus === 'error'">Error loading search results.</p>
+      <div v-if="loadStatus === 'loaded'" class="grid grid-cols-auto-md gap-4">
+        <p v-if="matches.length === 0">No results found.</p>
         <SearchResultCard
           v-for="match in matches"
           :key="match.objectId"
@@ -14,15 +18,27 @@
           :showDetails="false"
         />
       </div>
+      <div class="mt-8">
+        <Button
+          variant="primary"
+          class="btn btn-primary"
+          :disabled="loadStatus === 'loading'"
+          @click="loadMore"
+        >
+          Load more
+        </Button>
+      </div>
     </div>
   </DefaultLayout>
 </template>
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, watch } from "vue";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { SearchResultMatch } from "@/types";
 import api from "@/api";
 import SearchResultCard from "@/components/SearchResultCard/SearchResultCard.vue";
+import { SpinnerIcon } from "@/icons";
+import Button from "@/components/Button/Button.vue";
 
 const props = defineProps<{
   searchId: string;
@@ -31,25 +47,33 @@ const props = defineProps<{
 const matches = ref<SearchResultMatch[]>([]);
 const searchText = ref<string>("");
 const page = ref(0);
+const loadStatus = ref<"loading" | "loaded" | "error">("loading");
 
-onMounted(async () => {
-  const res = await api.getSearchResultsById(props.searchId);
-  searchText.value = res.searchEntry.searchText ?? "";
-  matches.value = res.matches;
-});
+watch(
+  () => props.searchId,
+  async () => {
+    loadStatus.value = "loading";
+    try {
+      const res = await api.getSearchResultsById(props.searchId);
+      searchText.value = res.searchEntry.searchText ?? "";
+      matches.value = res.matches;
+      loadStatus.value = "loaded";
+    } catch (err) {
+      loadStatus.value = "error";
+      console.error(err);
+    }
+  },
+  { immediate: true }
+);
 
 async function loadMore() {
+  loadStatus.value = "loading";
   page.value += 1;
   const res = await api.getSearchResultsById(props.searchId, page.value);
+  loadStatus.value = "loaded";
 
   const moreMatches = res.matches;
   matches.value = [...matches.value, ...moreMatches];
 }
 </script>
-<style scoped>
-.search-results {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
-  grid-gap: 1rem;
-}
-</style>
+<style scoped></style>
