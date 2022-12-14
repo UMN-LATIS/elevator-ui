afb4215b9d68d36a02ad58cbd308bcce2ee9a6f0
James Johnson <johnsojr@umn.edu>
2022-12-14 09:26:56 -0600

    make old default layout noscroll, new default is scrollable

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,46 +1,10 @@
 <template>
-  <div class="h-screen flex flex-col">
-    <AppHeader class="top-0 w-full z-20 backdrop-blur-sm" />
-    <div ref="contentContainer" class="flex-1 mt-18 md:mt-0 overflow-auto">
-      <slot />
-    </div>
+  <div class="min-h-screen pt-18">
+    <AppHeader class="top-0 w-full z-20 sticky left-0" />
+    <slot />
   </div>
 </template>
 <script setup lang="ts">
-import { ref, watch, nextTick } from "vue";
 import AppHeader from "@/components/AppHeader/AppHeader.vue";
-import { useScroll, useSessionStorage } from "@vueuse/core";
-import { onBeforeRouteUpdate, onBeforeRouteLeave, useRoute } from "vue-router";
-
-const contentContainer = ref<HTMLElement | undefined>();
-
-interface ScrollYHistory {
-  [route: string]: number;
-}
-
-const scrollYHistory = useSessionStorage<ScrollYHistory>("scrollY_history", {});
-const { y } = useScroll(contentContainer);
-const route = useRoute();
-
-watch(
-  route,
-  () => {
-    const newPathScrollY = scrollYHistory.value[route.path] ?? 0;
-    nextTick(() => {
-      contentContainer.value?.scrollTo(0, newPathScrollY);
-    });
-  },
-  { immediate: true }
-);
-
-onBeforeRouteUpdate((to, from) => {
-  console.log("before route update", { y: y.value, to, from });
-  scrollYHistory.value[from.path] = y.value;
-});
-
-onBeforeRouteLeave((to, from) => {
-  console.log("before route leave", { y: y.value, to, from });
-  scrollYHistory.value[from.path] = y.value;
-});
 </script>
 <style></style>
