b5610753769fc619cb37f327c531dc9e74bcbd20
James Johnson <johnsojr@umn.edu>
2023-03-28 16:41:10 -0500

    add some light formatting to content

--- src/pages/StaticContentPage/StaticContentPage.vue
+++ src/pages/StaticContentPage/StaticContentPage.vue
@@ -1,13 +1,22 @@
 <template>
   <DefaultLayout>
-    <div v-if="page" class="prose mx-auto pt-8">
-      <h1>{{ page.title }}</h1>
-      <div v-html="page.content"></div>
+    <div class="static-page-view max-w-full overflow-scroll">
+      <article
+        v-if="page"
+        class="static-page-view__article m-auto sm:max-w-3xl p-4 sm:p-12 rounded shadow sm:px-12 sm:my-8"
+      >
+        <h2 class="text-3xl mb-12 sm:text-5xl font-bold mt-4">
+          {{ page?.title }}
+        </h2>
+
+        <SanitizedHTML :html="page.content" class="prose prose-neutral" />
+      </article>
     </div>
   </DefaultLayout>
 </template>
 <script setup lang="ts">
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import SanitizedHTML from "@/components/SanitizedHTML/SanitizedHTML.vue";
 import { onMounted, ref } from "vue";
 import { ApiStaticPageResponse } from "@/types";
 import api from "@/api";
@@ -22,4 +31,13 @@ onMounted(async () => {
   page.value = await api.getStaticPage(props.pageId);
 });
 </script>
-<style scoped></style>
+<style scoped>
+.static-page-view__article {
+  background: var(--app-metaDataOnlyView-contentViewer-backgroundColor);
+  color: var(--app-metaDataOnlyView-contentViewer-textColor);
+}
+
+.static-page-view__article h2:after {
+  background: var(--app-metaDataOnlyView-contentViewer-textColor);
+}
+</style>
