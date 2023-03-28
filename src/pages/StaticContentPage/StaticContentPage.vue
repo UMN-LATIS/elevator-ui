d6c83faa11061668e9739975c62560d4ac1dce04
James Johnson <johnsojr@umn.edu>
2023-03-28 15:23:02 -0500

    Render page content

--- src/pages/StaticContentPage/StaticContentPage.vue
+++ src/pages/StaticContentPage/StaticContentPage.vue
@@ -1,14 +1,25 @@
 <template>
   <DefaultLayout>
-    <h1>Page {{ pageId }}</h1>
-    Static Content goes here
+    <div v-if="page" class="prose mx-auto pt-8">
+      <h1>{{ page.title }}</h1>
+      <div v-html="page.content"></div>
+    </div>
   </DefaultLayout>
 </template>
 <script setup lang="ts">
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import { onMounted, ref } from "vue";
+import { ApiStaticPageResponse } from "@/types";
+import api from "@/api";
 
-defineProps<{
+const props = defineProps<{
   pageId: string;
 }>();
+
+const page = ref<ApiStaticPageResponse | null>(null);
+
+onMounted(async () => {
+  page.value = await api.getStaticPage(props.pageId);
+});
 </script>
 <style scoped></style>
