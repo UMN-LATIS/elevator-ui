e9553229572a5d0fc789d8e2016e2e25e810b88b
James Johnson <johnsojr@umn.edu>
2023-03-28 14:46:23 -0500

    add route for static pages

---
+++ src/pages/StaticContentPage/StaticContentPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <DefaultLayout>
+    <h1>Page {{ pageId }}</h1>
+    Static Content goes here
+  </DefaultLayout>
+</template>
+<script setup lang="ts">
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+
+defineProps<{
+  pageId: string;
+}>();
+</script>
+<style scoped></style>
