df628b6e1ddf2332639103ebab751fd47eeb5d00
James Johnson <johnsojr@umn.edu>
2023-03-30 14:03:36 -0500

    refactor

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -6,40 +6,35 @@
 <script setup lang="ts">
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { ref, watch } from "vue";
-import { ApiStaticPageResponse } from "@/types";
+import { StaticContentPage } from "@/types";
 import api from "@/api";
 import { useInstanceStore } from "@/stores/instanceStore";
 import PageContentPure from "@/components/PageContentPure/PageContentPure.vue";
 
-const page = ref<ApiStaticPageResponse | null>(null);
-
+const page = ref<StaticContentPage | null>(null);
 const instanceStore = useInstanceStore();
+
+const fallbackHomePage: StaticContentPage = {
+  title: "No Home Page",
+  content: `<p>Update your instance to set a Home Page</p>`,
+};
+
+// the Home Page is just the first page with a title of "Home Page"
+function findHomePageId() {
+  return instanceStore.pages.find((page) => page.title === "Home Page")?.id;
+}
+
+async function fetchHomePage(homePageId: number | undefined) {
+  if (!homePageId) return fallbackHomePage;
+  return api.getStaticPage(homePageId);
+}
 
 watch(
   () => instanceStore.fetchStatus,
   async () => {
     if (instanceStore.fetchStatus !== "success") return;
-
-    // find the pageId of the Home Page
-    // the Home Page is just the first page with a title
-    // of "Home Page"
-    const homePageId = instanceStore.pages.find(
-      (page) => page.title === "Home Page"
-    )?.id;
-
-    if (!homePageId) {
-      // set the page content to something generic
-      console.error("No Home Page found");
-      page.value = {
-        title: "No Home Page",
-        content: `
-        <p>Update your instance to set a Home Page</p>
-        `,
-      };
-      return;
-    }
-
-    page.value = await api.getStaticPage(homePageId);
+    const homePageId = findHomePageId();
+    page.value = await fetchHomePage(homePageId);
   },
   { immediate: true }
 );
