0d19beafd6e93508cc720bdb4158adba77dffe71
James Johnson <johnsojr@umn.edu>
2023-04-05 11:57:18 -0500

    fix janky resizing

--- src/pages/CollectionsPage/CollectionsPage.vue
+++ src/pages/CollectionsPage/CollectionsPage.vue
@@ -2,7 +2,7 @@
   <DefaultLayout>
     <div class="collections-page p-8 px-4">
       <h1 class="text-4xl font-bold my-8">Collections</h1>
-      <div class="grid">
+      <div ref="collectionGrid" class="grid">
         <CollectionItem
           v-for="collection in collections"
           :key="collection.id"
@@ -13,15 +13,17 @@
   </DefaultLayout>
 </template>
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import CollectionItem from "./CollectionItem.vue";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { useInstanceStore } from "@/stores/instanceStore";
-import { useWindowSize } from "@vueuse/core";
+import { useResizeObserver } from "@vueuse/core";
 
 const instanceStore = useInstanceStore();
+const collectionGrid = ref<HTMLElement | null>(null);
 
 const collections = computed(() => instanceStore.collections);
+const numCols = ref(1);
 
 // by default, css grid will order the items by left-to-right,
 // then top-to-bottom. This makes is difficult to read:
@@ -30,15 +32,18 @@ const collections = computed(() => instanceStore.collections);
 // once we know the number of rows and columns, we can
 // use css grid and set grid-auto-flow to column
 // to achieve this.
-const { width } = useWindowSize();
-const numCols = computed(() => {
-  if (width.value < 640) return 1;
-  if (width.value < 768) return 2;
-  if (width.value < 1024) return 3;
-  return 4;
+useResizeObserver(collectionGrid, (entries) => {
+  const [entry] = entries;
+  const { width } = entry.contentRect;
+  if (width < 640) numCols.value = 1;
+  if (width < 768) numCols.value = 2;
+  if (width < 1024) numCols.value = 3;
+  if (width >= 1024) numCols.value = 4;
 });
+
 const numRows = computed(() => {
-  return Math.ceil(collections.value.length / numCols.value);
+  const numCollections = collections.value.length;
+  return Math.ceil(numCollections / numCols.value);
 });
 </script>
 <style scoped>
