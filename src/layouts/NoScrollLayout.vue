55d6f8a17b0ee06d4c7dd2e2fe6b29a2181937e4
James Johnson <johnsojr@umn.edu>
2023-04-10 15:56:30 -0500

    remove unneeded stuff

--- src/layouts/NoScrollLayout.vue
+++ src/layouts/NoScrollLayout.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="h-screen flex flex-col">
     <AppHeader class="top-0 w-full z-20 backdrop-blur-sm" />
-    <PrevNextSearchResultNav
-      v-if="searchStore.searchId"
-      class="border-b sticky"
-    />
+    <PrevNextSearchResultNav />
     <div ref="contentContainer" class="flex-1 mt-18 md:mt-0 overflow-auto">
       <slot />
     </div>
@@ -13,8 +10,5 @@
 <script setup lang="ts">
 import AppHeader from "@/components/AppHeader/AppHeader.vue";
 import PrevNextSearchResultNav from "@/components/PrevNextSearchResultNav/PrevNextSearchResultNav.vue";
-import { useSearchStore } from "@/stores/searchStore";
-
-const searchStore = useSearchStore();
 </script>
 <style></style>
