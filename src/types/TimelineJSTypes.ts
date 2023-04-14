e933fe4b0621ad29e4a058c5bacb93ca70af8d15
James Johnson <johnsojr@umn.edu>
2023-04-14 13:52:45 -0500

    Add options

--- src/types/TimelineJSTypes.ts
+++ src/types/TimelineJSTypes.ts
@@ -44,3 +44,41 @@ export interface TimelineJSJSON {
   eras?: TimelineJSEra[];
   scale?: "human" | "cosmological"; // default is "human"
 }
+
+export interface TimelineJSOptions {
+  font?: "default" | string; // default is "default"
+  debug?: boolean; // default is false
+  height?: number;
+  width?: number;
+  is_embed?: boolean; // default is false
+  hash_bookmark?: boolean; // default is false
+  default_bg_color?: string;
+  scale_factor?: number;
+  initial_zoom?: number;
+  zoom_sequence?: number[];
+  timenav_position?: "top" | "bottom"; // default is "bottom"
+  optimal_tick_width?: number; // 100
+  base_class?: string; // "tl-timeline"
+  timenav_height?: number; // 150
+  timenav_height_percentage?: number; // 25
+  timenav_mobile_height_percentage?: number; // 40
+  timenav_height_min?: number; // 150
+  marker_height_min?: number; // 30
+  marker_width_min?: number; // 100
+  marker_padding?: number; // 5
+  start_at_slide?: number; // 0
+  start_at_end?: boolean; // false
+  menubar_height?: number; // 0
+  use_bc?: boolean; // false
+  duration?: number; // 1000
+  ease?: string; // "easeInOutQuint"
+  dragging?: boolean; // true
+  trackResize?: boolean; // true
+  slide_padding_lr?: number; // 100
+  slide_default_fade?: string; // "0%"
+  language?: string; // "en"
+  ga_property_id?: string;
+  track_events?: string[];
+  script_path?: string;
+  soundcite?: boolean; // false
+}
