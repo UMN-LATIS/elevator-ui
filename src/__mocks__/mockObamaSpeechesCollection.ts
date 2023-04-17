875d696d78e2177bf4e7fc17c154101b6fe49300
James Johnson <johnsojr@umn.edu>
2023-04-17 14:25:48 -0500

    add mock obama speeches collection for testing

--- src/__mocks__/mockObamaSpeechesCollection.ts
+++ src/__mocks__/mockObamaSpeechesCollection.ts
@@ -1,5 +1,75 @@
 import type { SearchResultMatch, TimelineJSSlide } from "@/types";
 
+export const africaLeadersSpeechMatch: SearchResultMatch = {
+  dates: [
+    {
+      label: "Date",
+      dateAsset: [
+        {
+          end: {
+            text: "",
+            numeric: "",
+          },
+          label: "",
+          start: {
+            text: "08/05/2014",
+            numeric: "1407196800",
+          },
+          isPrimary: true,
+        },
+      ],
+    },
+  ],
+  title: "Africa Leaders Speech",
+  entries: [
+    {
+      Date: true,
+      label: "Date",
+      entries: ["08/05/2014"],
+    },
+  ],
+  objectId: "58bb0779ba98a87e062c0740",
+  locations: [],
+  primaryHandlerId: "58bb07c8ba98a8242c8b4574",
+  primaryHandlerType: "AudioHandler",
+  primaryHandlerThumbnail:
+    "//elevatorbetabucket.s3.amazonaws.com/thumbnail/4754b8c2428a89ab8c70bb85-thumbnail",
+  primaryHandlerThumbnail2x:
+    "//elevatorbetabucket.s3.amazonaws.com/thumbnail/4754b8c2428a89ab8c70bb85-thumbnail2x",
+  collectionHierarchy: [
+    {
+      id: 35,
+      title: "Obama Speeches",
+    },
+  ],
+  template: {
+    name: "Speech",
+    id: 49,
+  },
+  base_url: "/defaultinstance/",
+  isChild: false,
+  hasChildren: false,
+};
+
+export const africaLeadersSpeechSlide: TimelineJSSlide = {
+  start_date: {
+    display_date: "08/05/2014",
+    year: 2014,
+    month: 8,
+    day: 3,
+  },
+  text: {
+    headline:
+      '<a href="/defaultinstance/asset/viewAsset/58bb0779ba98a87e062c0740">Africa Leaders Speech</a>',
+    text: '\n\n<div class="previewEntry"> <strong>Date:</strong><ul>\n\t\t\t\n\t\t\t\t<li>08/05/2014</li>\n\t\t\t\n\t\t</ul>\n</div>\n\n',
+  },
+  media: {
+    thumbnail:
+      "//elevatorbetabucket.s3.amazonaws.com/thumbnailnail/4754b8c2428a89ab8c70bb85-thumbnailnail2x",
+    url: "//elevatorbetabucket.s3.amazonaws.com/thumbnailnail/4754b8c2428a89ab8c70bb85-thumbnailnail2x",
+  },
+};
+
 export const mockSearchResults: SearchResultMatch[] = [
   {
     dates: [],
