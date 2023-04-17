d438ff56829432249bc3f7417586352149f507e7
James Johnson <johnsojr@umn.edu>
2023-04-17 14:27:35 -0500

    add helpers for converting search result to slide

--- src/helpers/timelineHelpers.test.ts
+++ src/helpers/timelineHelpers.test.ts
@@ -1,15 +1,141 @@
 import { describe, expect, it } from "@jest/globals";
-import { convertMatchToTimelineJSSlide } from "./timelineHelpers";
 import {
-  mockSearchResults,
-  mockTimelineJSSlides,
-} from "@/__mocks__/mockObamaSpeechesCollection";
+  toTimelineJSSlide,
+  convertTextToDateObject,
+  convertTimestampToDateObject,
+  toDateObject,
+} from "./timelineHelpers";
+import { SearchResultMatch, TimelineJSSlide } from "@/types";
 
 describe("timelineHelpers", () => {
-  it("should convert a search result match to a timelineJS slide", () => {
-    const slides = mockSearchResults
-      .map(convertMatchToTimelineJSSlide)
-      .filter(Boolean);
-    expect(slides).toEqual(mockTimelineJSSlides);
+  describe("toDateObject", () => {
+    it("converts a Date to a DateObject", () => {
+      const date = new Date("08/05/2014");
+      const dateObject = toDateObject(date);
+
+      expect(dateObject).toEqual({
+        display_date: "8/5/2014",
+        year: 2014,
+        month: 8,
+        day: 5,
+      });
+    });
+  });
+
+  describe("convertDateStringToObject", () => {
+    it("converts a MM-DD-YYYY date string to a DateObject", () => {
+      const dateText = "08-05-2014";
+      const dateObject = convertTextToDateObject(dateText);
+
+      expect(dateObject).toEqual({
+        display_date: "8/5/2014",
+        year: 2014,
+        month: 8,
+        day: 5,
+      });
+    });
+
+    it("converts a MM/DD/YYYY date string to a DateObject", () => {
+      const dateText = "08/05/2014";
+      const dateObject = convertTextToDateObject(dateText);
+
+      expect(dateObject).toEqual({
+        display_date: "8/5/2014",
+        year: 2014,
+        month: 8,
+        day: 5,
+      });
+    });
+  });
+
+  describe("convertDateTimestampToObject", () => {
+    it("converts a timestamp to a DateObject", () => {
+      const timestamp = 1407196800;
+      const dateObject = convertTimestampToDateObject(timestamp);
+
+      expect(dateObject).toEqual({
+        display_date: "8/5/2014",
+        year: 2014,
+        month: 8,
+        day: 5,
+      });
+    });
+  });
+
+  describe("toTimelineJSSlide", () => {
+    it("should convert a search result match to a timelineJS slide", () => {
+      const africaLeadersSpeechMatch: SearchResultMatch = {
+        dates: [
+          {
+            label: "Date",
+            dateAsset: [
+              {
+                end: {
+                  text: "",
+                  numeric: "",
+                },
+                label: "",
+                start: {
+                  text: "08/05/2014",
+                  numeric: "1407196800",
+                },
+                isPrimary: true,
+              },
+            ],
+          },
+        ],
+        title: "Africa Leaders Speech",
+        entries: [
+          {
+            Date: true,
+            label: "Date",
+            entries: ["08/05/2014"],
+          },
+        ],
+        objectId: "58bb0779ba98a87e062c0740",
+        locations: [],
+        primaryHandlerId: "58bb07c8ba98a8242c8b4574",
+        primaryHandlerType: "AudioHandler",
+        primaryHandlerThumbnail:
+          "//elevatorbetabucket.s3.amazonaws.com/thumbnail/4754b8c2428a89ab8c70bb85-thumbnail",
+        primaryHandlerThumbnail2x:
+          "//elevatorbetabucket.s3.amazonaws.com/thumbnail/4754b8c2428a89ab8c70bb85-thumbnail2x",
+        collectionHierarchy: [
+          {
+            id: 35,
+            title: "Obama Speeches",
+          },
+        ],
+        template: {
+          name: "Speech",
+          id: 49,
+        },
+        base_url: "/defaultinstance/",
+        isChild: false,
+        hasChildren: false,
+      };
+
+      const africaLeadersSpeechSlide: TimelineJSSlide = {
+        start_date: {
+          display_date: "8/5/2014",
+          year: 2014,
+          month: 8,
+          day: 5,
+        },
+        text: {
+          headline:
+            '<a href="/defaultinstance/asset/viewAsset/58bb0779ba98a87e062c0740">Africa Leaders Speech</a>',
+          text: `<div class=\"previewEntry\"><strong>Date:</strong><ul><li>08/05/2014</li></ul></div>`,
+        },
+        media: {
+          thumbnail:
+            "//elevatorbetabucket.s3.amazonaws.com/thumbnail/4754b8c2428a89ab8c70bb85-thumbnail2x",
+          url: "//elevatorbetabucket.s3.amazonaws.com/thumbnail/4754b8c2428a89ab8c70bb85-thumbnail2x",
+        },
+      };
+      expect(toTimelineJSSlide(africaLeadersSpeechMatch)).toEqual(
+        africaLeadersSpeechSlide
+      );
+    });
   });
 });
--- src/helpers/timelineHelpers.ts
+++ src/helpers/timelineHelpers.ts
@@ -1,8 +1,72 @@
 import type { SearchResultMatch, TimelineJSSlide } from "@/types";
-import { legacyPrepTimeline } from "@/helpers/legacyPrepTimeline";
 
-export function convertMatchToTimelineJSSlide(
-  match: SearchResultMatch
-): TimelineJSSlide | null {
-  return legacyPrepTimeline(match);
+export type DateObject = {
+  display_date?: string;
+  year: number;
+  month: number;
+  day: number;
+};
+
+// converts a UTC date to a DateObject
+export function toDateObject(utcDate: Date): DateObject {
+  return {
+    display_date: utcDate.toLocaleDateString("en-US", { timeZone: "UTC" }),
+    year: utcDate.getUTCFullYear(),
+    month: utcDate.getUTCMonth() + 1, // Months are zero-based, so we need to add 1
+    day: utcDate.getUTCDate(),
+  };
+}
+
+export function convertTextToDateObject(dateText: string): DateObject | null {
+  const date = new Date(dateText);
+
+  if (isNaN(date.getTime())) {
+    return null;
+  }
+
+  return toDateObject(date);
+}
+
+export function convertTimestampToDateObject(
+  timestamp: number | string
+): DateObject {
+  const numericTimestamp =
+    typeof timestamp === "string" ? Number.parseInt(timestamp) : timestamp;
+
+  const date = new Date(numericTimestamp * 1000);
+  return toDateObject(date);
+}
+
+export function toTimelineJSSlide(match: SearchResultMatch): TimelineJSSlide {
+  const dateAsset = match.dates[0].dateAsset[0];
+
+  const start_date = convertTimestampToDateObject(dateAsset.start.numeric);
+  const end_date = dateAsset.end?.numeric
+    ? convertTimestampToDateObject(dateAsset.end.numeric)
+    : undefined;
+
+  const headline = `<a href="/defaultinstance/asset/viewAsset/${match.objectId}">${match.title}</a>`;
+
+  const dateEntries = match.entries?.flatMap((entry) => entry.entries) ?? [];
+
+  const text = `<div class="previewEntry"><strong>Date:</strong><ul>${dateEntries
+    .map((entry) => `<li>${entry}</li>`)
+    .join("")}</ul></div>`;
+
+  const media = match.primaryHandlerThumbnail2x
+    ? {
+        thumbnail: match.primaryHandlerThumbnail2x,
+        url: match.primaryHandlerThumbnail2x,
+      }
+    : undefined;
+
+  return {
+    start_date,
+    end_date,
+    text: {
+      headline,
+      text,
+    },
+    media,
+  };
 }
