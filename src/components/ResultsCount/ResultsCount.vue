b4b14feda96f142e3788d24cc7df682cc1eab46c
James Johnson <johnsojr@umn.edu>
2023-04-18 09:55:07 -0500

    create a standard results count component

---
+++ src/components/ResultsCount/ResultsCount.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="results-count">
+    <div v-if="total > 0" class="flex gap-2 items-baseline">
+      <p class="text-sm text-neutral-500">
+        <b>{{ showingCount }}</b> of <b>{{ total }}</b> results
+      </p>
+      <Button
+        v-if="showingCount < total && status !== 'fetching'"
+        variant="tertiary"
+        @click="$emit('loadMore')"
+      >
+        Load More
+      </Button>
+      <SpinnerIcon
+        v-if="status === 'fetching'"
+        class="w-3 h-3 text-neutral-500"
+      />
+    </div>
+    <p
+      v-if="status !== 'fetching' && total === 0"
+      class="text-sm text-neutral-500"
+    >
+      No results found.
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+import Button from "@/components/Button/Button.vue";
+import SpinnerIcon from "@/icons/SpinnerIcon.vue";
+import type { FetchStatus } from "@/types";
+defineProps<{
+  showingCount: number;
+  total: number;
+  status: FetchStatus;
+}>();
+
+defineEmits<{
+  (event: "loadMore");
+}>();
+</script>
+<style scoped></style>
