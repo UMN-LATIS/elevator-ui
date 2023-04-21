b654945da0cf19679b26731269a73cd59d0506af
James Johnson <johnsojr@umn.edu>
2023-04-21 09:32:03 -0500

    refactor

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -24,10 +24,16 @@
 import { ref, watch, provide, onMounted, onUnmounted, reactive } from "vue";
 import { useResizeObserver } from "@vueuse/core";
 import "maplibre-gl/dist/maplibre-gl.css";
-import { Map as MapLibreMap, MapMouseEvent, GeoJSONSource } from "maplibre-gl";
+import {
+  Map as MapLibreMap,
+  MapMouseEvent,
+  GeoJSONSource,
+  FullscreenControl,
+  GeolocateControl,
+  ScaleControl,
+} from "maplibre-gl";
 import { LngLat, BoundingBox, MapContext, AddMarkerArgs } from "@/types";
 import { MapInjectionKey } from "@/constants/mapConstants";
-import { withMapControls } from "./withMapControls";
 
 const props = defineProps<{
   center: LngLat;
@@ -119,100 +125,122 @@ function renderMarkers() {
 
 watch(activeMapStyleKey, updateStyle);
 watch([() => props.bounds, mapRef], updateBounds);
-watch([mapRef, markers], renderMarkers);
+watch([mapRef, markers, activeMapStyleKey], renderMarkers);
 
 onMounted(() => {
   if (!mapContainerRef.value) {
     throw Error("Cannot create Map: container not defined:");
   }
 
+  const map = new MapLibreMap({
+    container: mapContainerRef.value,
+    center: props.center ? [props.center.lng, props.center.lat] : [0, 0],
+    style: getArcGISUrl(activeMapStyleKey.value),
+    zoom: props.zoom,
+  })
+    .addControl(
+      new FullscreenControl({
+        container: document.querySelector("body") as HTMLBodyElement,
+      })
+    )
+    .addControl(
+      new GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        // showUserHeading: true,
+      })
+    )
+    .addControl(new ScaleControl({ unit: "imperial" }))
+    .on("click", (event: MapMouseEvent) => {
+      if (!mapRef.value) {
+        throw new Error("there was a click but no map");
+      }
+
+      emit("click", event, mapRef.value as unknown as MapLibreMap);
+    })
+    .on("load", () => {
+      if (!mapRef.value) {
+        throw new Error("cannot emit load event: no map");
+      }
+      emit("load", mapRef.value as unknown as MapLibreMap);
+
+      // Add a new GeoJSON source with clustering enabled
+      mapRef.value.addSource("markers", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: Array.from(markers.values()),
+        },
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50,
+      });
+
+      // Add a layer for clustered points
+      mapRef.value.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "markers",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            10,
+            "#f1f075",
+            100,
+            "#f28cb1",
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40,
+          ],
+        },
+      });
+
+      mapRef.value.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "markers",
+        layout: {
+          "text-font": ["Arial Bold"],
+          "text-field": ["get", "point_count"],
+          "text-offset": [0, 0.1], // move the label vertically downwards slightly to improve centering
+        },
+        paint: {
+          "text-color": "black",
+        },
+      });
+
+      // Add a layer for individual points
+      mapRef.value.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "markers",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#f43f5e",
+          "circle-radius": 10,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff",
+        },
+      });
+    });
+
   // added to avoid ts warning about deep nesting
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  mapRef.value = withMapControls(
-    new MapLibreMap({
-      container: mapContainerRef.value,
-      center: props.center ? [props.center.lng, props.center.lat] : [0, 0],
-      style: getArcGISUrl(activeMapStyleKey.value),
-      zoom: props.zoom,
-    })
-  );
-
-  // add click handler
-  mapRef.value.on("click", (event: MapMouseEvent) => {
-    if (!mapRef.value) {
-      throw new Error("there was a click but no map");
-    }
-
-    emit("click", event, mapRef.value as unknown as MapLibreMap);
-  });
-
-  mapRef.value.on("load", () => {
-    if (!mapRef.value) {
-      throw new Error("cannot emit load event: no map");
-    }
-    emit("load", mapRef.value as unknown as MapLibreMap);
-
-    // Add a new GeoJSON source with clustering enabled
-    mapRef.value.addSource("markers", {
-      type: "geojson",
-      data: {
-        type: "FeatureCollection",
-        features: Array.from(markers.values()),
-      },
-      cluster: true,
-      clusterMaxZoom: 14,
-      clusterRadius: 50,
-    });
-
-    // Add a layer for clustered points
-    mapRef.value.addLayer({
-      id: "clusters",
-      type: "circle",
-      source: "markers",
-      filter: ["has", "point_count"],
-      paint: {
-        "circle-color": [
-          "step",
-          ["get", "point_count"],
-          "#51bbd6",
-          10,
-          "#f1f075",
-          100,
-          "#f28cb1",
-        ],
-        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
-      },
-    });
-
-    mapRef.value.addLayer({
-      id: "cluster-count",
-      type: "symbol",
-      source: "markers",
-      layout: {
-        "text-font": ["Arial Bold"],
-        "text-field": ["get", "point_count"],
-        "text-offset": [0, 0.1], // move the label vertically downwards slightly to improve centering
-      },
-      paint: {
-        "text-color": "black",
-      },
-    });
-
-    // Add a layer for individual points
-    mapRef.value.addLayer({
-      id: "unclustered-point",
-      type: "circle",
-      source: "markers",
-      filter: ["!", ["has", "point_count"]],
-      paint: {
-        "circle-color": "#f43f5e",
-        "circle-radius": 10,
-        "circle-stroke-width": 1,
-        "circle-stroke-color": "#fff",
-      },
-    });
-  });
+  mapRef.value = map;
 
   useResizeObserver(mapContainerRef, () => {
     if (!mapRef.value) return;
