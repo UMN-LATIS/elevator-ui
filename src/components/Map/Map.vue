da5049fa96fc83e0d06799ad75cb2ef0d0d2691a
James Johnson <johnsojr@umn.edu>
2023-04-21 09:54:11 -0500

    cleanup

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -148,10 +148,7 @@ onMounted(() => {
         positionOptions: {
           enableHighAccuracy: true,
         },
-        // When active the map will receive updates to the device's location as it changes.
         trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
-        // showUserHeading: true,
       })
     )
     .addControl(new ScaleControl({ unit: "imperial" }))
