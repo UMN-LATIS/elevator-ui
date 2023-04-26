4bd8779c8ef5ca4dec5d33333980d66e806487a5
James Johnson <johnsojr@umn.edu>
2023-04-26 11:47:18 -0500

    wip

--- src/components/SearchResultsGallery/SearchResultsGallery.vue
+++ src/components/SearchResultsGallery/SearchResultsGallery.vue
@@ -1,7 +1,74 @@
 <template>
   <div class="search-results-gallery">
-    <h1>Gallery Goes Here</h1>
+    <h1>Gallery</h1>
+
+    <Swiper
+      :modules="modules"
+      :slidesPerView="1"
+      :spaceBetween="50"
+      navigation
+      :pagination="{ clickable: true }"
+      :scrollbar="{ draggable: true }"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+
+      <SwiperSlide>Slide 2</SwiperSlide>
+
+      <SwiperSlide>Slide 2</SwiperSlide>
+
+      <SwiperSlide>Slide 2</SwiperSlide>
+    </Swiper>
   </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped></style>
+<script setup lang="ts">
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const modules = [Navigation, Pagination, Scrollbar, A11y];
+
+const onSwiper = (swiper) => {
+  console.log(swiper);
+};
+const onSlideChange = () => {
+  console.log("slide change");
+};
+</script>
+<style>
+.swiper {
+  width: 100%;
+  height: 75vh;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
