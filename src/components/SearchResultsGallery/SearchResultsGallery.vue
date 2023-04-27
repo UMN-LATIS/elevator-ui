5a345550a0a57e75eb2407cd24835da8ad5638b9
James Johnson <johnsojr@umn.edu>
2023-04-27 11:37:27 -0500

    swiper no swiping

--- src/components/SearchResultsGallery/SearchResultsGallery.vue
+++ src/components/SearchResultsGallery/SearchResultsGallery.vue
@@ -1,44 +1,121 @@
+ty
 <template>
   <div class="search-results-gallery">
     <h1>Gallery</h1>
 
+    <!-- Main Swiper -> pass thumbs swiper instance -->
     <Swiper
+      class="main-swiper"
       :modules="modules"
       :slidesPerView="1"
       :spaceBetween="50"
       navigation
       :pagination="{ clickable: true }"
       :scrollbar="{ draggable: true }"
+      :thumbs="{ swiper: thumbsSwiper }"
       @swiper="onSwiper"
       @slideChange="onSlideChange"
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-
-      <SwiperSlide>Slide 2</SwiperSlide>
-
-      <SwiperSlide>Slide 2</SwiperSlide>
-
-      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide v-for="(slide, i) in slides" :key="i">
+        <div class="flex items-center justify-center w-full">
+          <LazyLoadImage
+            v-if="slide.thumb.src"
+            :src="slide.thumb.src"
+            :alt="slide.thumb.alt"
+            class="swiper-lazy h-full"
+          />
+          <DocumentIcon v-else />
+        </div>
+      </SwiperSlide>
     </Swiper>
+
+    <!-- Thumbs Swiper -> store swiper instance -->
+    <!-- It is also required to set watchSlidesProgress prop -->
+    <swiper
+      :modules="[Thumbs]"
+      watchSlidesProgress
+      :slidesPerView="10"
+      :spaceBetween="4"
+      @swiper="setThumbsSwiper"
+    >
+      <SwiperSlide v-for="(slide, i) in slides" :key="i">
+        <div
+          class="border border-neutral-400 aspect-video flex items-center justify-center w-full"
+        >
+          <LazyLoadImage
+            v-if="slide.thumb.src"
+            :src="slide.thumb.src"
+            :alt="slide.thumb.alt"
+            class="swiper-lazy object-cover w-full h-full"
+          />
+          <DocumentIcon v-else />
+        </div>
+      </SwiperSlide>
+    </swiper>
   </div>
 </template>
 <script setup lang="ts">
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-
-// Import Swiper Vue.js components
+import { ref, computed } from "vue";
 import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation, Pagination, Scrollbar, A11y, Thumbs } from "swiper";
+import { SearchResultMatch } from "@/types";
+import DocumentIcon from "@/icons/DocumentIcon.vue";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import { getThumbURL } from "@/helpers/displayUtils";
+import LazyLoadImage from "../LazyLoadImage/LazyLoadImage.vue";
 
-const modules = [Navigation, Pagination, Scrollbar, A11y];
+const props = defineProps<{
+  totalResults: number;
+  matches: SearchResultMatch[];
+  status: string;
+}>();
+
+defineEmits<{
+  (event: "loadMore");
+}>();
+
+const modules = [Navigation, Pagination, Scrollbar, A11y, Thumbs];
+const thumbsSwiper = ref(null);
+
+interface Slide {
+  title: string;
+  thumb: {
+    src: string | null;
+    alt: string;
+  };
+}
+const slides = computed((): Slide[] =>
+  props.matches.map(
+    (match): Slide => ({
+      title: selectTitleFromMatch(match),
+      thumb: {
+        src: selectThumbSrc(match),
+        alt: selectTitleFromMatch(match),
+      },
+    })
+  )
+);
+const selectTitleFromMatch = (match: SearchResultMatch) => {
+  const noTitleText = "No Title";
+  if (Array.isArray(match.title)) {
+    return match.title?.[0] ?? noTitleText;
+  }
+  return match.title ?? noTitleText;
+};
+
+const selectThumbSrc = (match: SearchResultMatch) => {
+  const { primaryHandlerId } = match;
+  return primaryHandlerId ? getThumbURL(primaryHandlerId) : null;
+};
+
+const setThumbsSwiper = (swiper) => {
+  console.log(swiper);
+  thumbsSwiper.value = swiper;
+};
 
 const onSwiper = (swiper) => {
   console.log(swiper);
@@ -48,9 +125,9 @@ const onSlideChange = () => {
 };
 </script>
 <style>
-.swiper {
+.main-swiper {
   width: 100%;
-  height: 75vh;
+  height: 50vh;
 }
 
 .swiper-slide {
