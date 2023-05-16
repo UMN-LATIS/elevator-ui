88981d09405d09e29701c51a62be8354fa6b6555
James Johnson <johnsojr@umn.edu>
2023-05-16 08:31:21 -0500

    Extend content area to bottom on static pages (#136)

--- src/pages/StaticContentPage/StaticContentPage.vue
+++ src/pages/StaticContentPage/StaticContentPage.vue
@@ -2,7 +2,7 @@
   <DefaultLayout>
     <div
       v-if="page"
-      class="static-page__content prose prose-neutral p-4 lg:p-8 mx-auto"
+      class="static-page__content prose prose-neutral p-4 lg:p-8 mx-auto flex-1"
     >
       <h1 class="text-4xl font-bold">
         {{ page.title || "No Title" }}
