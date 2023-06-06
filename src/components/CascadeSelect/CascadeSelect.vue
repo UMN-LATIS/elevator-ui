9a73975760e99bedd4abd0ec9c0324d08f4a7351
James Johnson <johnsojr@umn.edu>
2023-06-06 09:35:12 -0500

    handle cascade select changes

--- src/components/CascadeSelect/CascadeSelect.vue
+++ src/components/CascadeSelect/CascadeSelect.vue
@@ -1,65 +1,119 @@
 <template>
-  <div class="">
-    <label v-for="(selected, index) in listOfSelected" :key="index">
-      {{ getLabel(options) }}
-      <select v-model="listOfSelected[index]">
-        <option value="" disabled>Select a {{ getLabel(options) }}</option>
-        <option v-for="opt in getOpts(options)" :key="opt" :value="opt">
+  <div class="flex flex-col gap-4">
+    <div
+      v-for="(selected, segmentLevel) in listOfSelected"
+      :key="segmentLevel"
+      class="flex flex-col gap-1"
+    >
+      <label class="uppercase text-xs font-medium tracking-wider">
+        {{ selected.label }}
+      </label>
+      <select
+        class="rounded-md"
+        :value="selected.value"
+        @change="
+          handleSelectChange(
+            segmentLevel,
+            ($event.target as HTMLSelectElement).value
+          )
+        "
+      >
+        <option value="" disabled>Select a {{ selected.label }}</option>
+        <option v-for="opt in selected.options" :key="opt" :value="opt">
           {{ opt }}
         </option>
       </select>
-    </label>
-    <div>
-      <p>
-        <code>options: {{ options }}</code>
-      </p>
-      <p>
-        <code>listOfSelected: {{ listOfSelected }}</code>
-      </p>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { reactive } from "vue";
+import { path } from "ramda";
+import { reactive, computed } from "vue";
 
 interface CascaderSelectOptions {
   [label: string]: string[] | CascaderSelectOptions;
 }
 
-defineProps<{
+const props = defineProps<{
   options: CascaderSelectOptions;
 }>();
 
-const listOfSelected = reactive<string[]>([""]);
+interface SelectedSegment {
+  label: string;
+  options: string[];
+  value: string;
+}
 
-const getLabel = (options: CascaderSelectOptions): string => {
+const listOfSelected = reactive<SelectedSegment[]>([
+  {
+    label: getFirstKey(props.options),
+    options: getFirstOptions(props.options),
+    value: "",
+  },
+]);
+
+function getFirstKey(options: CascaderSelectOptions): string {
   return Object.keys(options)[0];
-};
+}
 
-const getOpts = (options: CascaderSelectOptions): string[] => {
-  const values = Object.values(options);
+function getFirstOptions(options: CascaderSelectOptions): string[] {
+  const values: Record<string, unknown> | string[] =
+    options[getFirstKey(options)];
 
-  // option values could look like:
-  // [['minneapolis', 'st. paul']]
-  // or could be another level of nesting
-  // [{ 'minneapolis': ...}, {'st. paul': ... }]
-  // let's use the first value to determine which is which
-
-  // if there's no first value, return an empty array
-  if (values.length === 0) {
-    return [];
+  // if the first value is an array, then
+  // values looks like: ['minneapolis', 'st. paul']
+  if (Array.isArray(values)) {
+    return values;
   }
 
-  // if the first value is an object, then
-  // values looks like
-  // [{ 'minneapolis': ...}, { 'st. paul': ... }]
-  // and so we want the keys of each object within the array
-  if (!Array.isArray(values[0])) {
-    return values.map((obj) => Object.keys(obj)[0]);
+  // otherwise, values is a record object like:
+  // { 'minneapolis': ..., 'st. paul': ... }
+  // and so the keys are our next level of options
+  return Object.keys(values);
+}
+
+function handleSelectChange(segmentLevel: number, value: string): void {
+  // update the value of the current segment
+  listOfSelected[segmentLevel].value = value;
+
+  // remove any segments after this one
+  listOfSelected.splice(segmentLevel + 1);
+
+  // if the value is empty, then we're done
+  if (!value) {
+    return;
   }
 
-  // otherwise, values looks like [['minneapolis', 'st. paul']]
-  return values[0];
-};
+  // otherwise, we need to add a new segment to the list
+  // get the current path by joining the labels and values
+  const currentPath: string[] = [];
+  listOfSelected.forEach((segment) => {
+    currentPath.push(segment.label);
+    currentPath.push(segment.value);
+  });
+
+  // the `path` function below takes an array of keys
+  // and returns the value at that path in the object:
+  // path(['a', 'b'], { a: { b: 2 } }); //=> 2
+  const nextSegmentOptions = path<CascaderSelectOptions>(
+    currentPath,
+    props.options
+  );
+
+  // if we couldn't find another segment at that path
+  // we're at the end of the road, so we're done
+  if (!nextSegmentOptions) {
+    return;
+  }
+
+  // otherwise add the next segment to the list of selected
+  const nextSegment: SelectedSegment = {
+    label: getFirstKey(nextSegmentOptions),
+    options: getFirstOptions(nextSegmentOptions),
+    value: "",
+  };
+
+  listOfSelected.push(nextSegment);
+}
 </script>
 <style scoped></style>
