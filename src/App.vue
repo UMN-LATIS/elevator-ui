ba535325d48898f4ea41fa245407c5e0a3338fff
James Johnson <johnsojr@umn.edu>
2023-06-23 11:20:41 -0500

    add error modal to app
    so that it's available on every page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <router-view v-if="instanceStore.isReady" />
+  <div>
+    <Teleport to="body">
+      <ErrorModal />
+    </Teleport>
+    <RouterView v-if="instanceStore.isReady" />
+  </div>
 </template>
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { useInstanceStore } from "./stores/instanceStore";
 import { useTheming } from "./helpers/useTheming";
+import ErrorModal from "@/components/ErrorModal/ErrorModal.vue";
 
 // load instance store before mounting app
 // this prevents a race conditiion where the search store
