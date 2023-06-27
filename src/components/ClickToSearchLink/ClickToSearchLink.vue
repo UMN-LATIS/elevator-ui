c4191b22e2d268d46109c0fd7191a53e1de9eff9
James Johnson <johnsojr@umn.edu>
2023-06-27 13:57:39 -0500

    reset search store on click

--- src/components/ClickToSearchLink/ClickToSearchLink.vue
+++ src/components/ClickToSearchLink/ClickToSearchLink.vue
@@ -15,6 +15,7 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
 import { WidgetProps } from "@/types";
+import { useSearchStore } from "@/stores/searchStore";
 import api from "@/api";
 
 interface Props {
@@ -24,8 +25,12 @@ interface Props {
 
 const props = defineProps<Props>();
 const router = useRouter();
+const searchStore = useSearchStore();
 
 async function handleClick() {
+  // clear search store
+  searchStore.reset();
+
   const searchId = await api.getSearchIdForClickToSearch(
     props.linkText,
     props.widget
