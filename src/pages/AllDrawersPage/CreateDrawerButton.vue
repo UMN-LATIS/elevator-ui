b4ce1087d3f28850a0fb322ca7471bc29bf8ad90
James Johnson <johnsojr@umn.edu>
2023-06-28 20:00:32 -0500

    trim title, check for uniqueness, add clear button

--- src/pages/AllDrawersPage/CreateDrawerButton.vue
+++ src/pages/AllDrawersPage/CreateDrawerButton.vue
@@ -7,33 +7,72 @@
     @close="isCreateDrawerModalOpen = false"
   >
     <form class="flex flex-col gap-6" @submit.prevent="handleCreateDrawer">
-      <InputGroup
-        id="new-drawer-title"
-        v-model="newDrawerTitle"
-        label="Title"
-      />
-      <Button type="submit" variant="primary" :disabled="!newDrawerTitle">
+      <div>
+        <InputGroup
+          id="new-drawer-title"
+          v-model="newDrawerTitle"
+          label="Title"
+          @input.once="isTitleTouched = true"
+        >
+          <template #append>
+            <button
+              v-if="newDrawerTitle.length"
+              type="button"
+              class="text-transparent-black-500 hover:text-neutral-900 float-right"
+              @click="resetForm"
+            >
+              <span class="sr-only">Clear</span>
+              <CircleXIcon />
+            </button>
+          </template>
+        </InputGroup>
+        <p
+          v-if="!isTitleValid && isTitleTouched"
+          class="text-sm text-red-600 mt-2"
+        >
+          Drawer title must be unique.
+        </p>
+      </div>
+      <Button type="submit" variant="primary" :disabled="!isTitleValid">
         Create
       </Button>
     </form>
   </Modal>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import Button from "@/components/Button/Button.vue";
 import Modal from "@/components/Modal/Modal.vue";
 import InputGroup from "@/components/InputGroup/InputGroup.vue";
 import { useDrawerStore } from "@/stores/drawerStore";
+import { CircleXIcon } from "@/icons";
 
 const isCreateDrawerModalOpen = ref(false);
 const newDrawerTitle = ref("");
+const isTitleTouched = ref(false);
 const drawerStore = useDrawerStore();
 
+const trimmedTitle = computed(() => newDrawerTitle.value.trim());
+const isTitleNonEmpty = computed(() => newDrawerTitle.value.length > 0);
+const isTitleUnique = computed(() => {
+  return !drawerStore.drawers.some(
+    (drawer) => drawer.title.trim() === trimmedTitle.value
+  );
+});
+
+const isTitleValid = computed(() => {
+  return isTitleNonEmpty.value && isTitleUnique.value;
+});
+
 function handleCreateDrawer() {
-  drawerStore.createDrawer(newDrawerTitle.value);
+  drawerStore.createDrawer(trimmedTitle.value);
   isCreateDrawerModalOpen.value = false;
+  resetForm();
+}
+
+function resetForm() {
   newDrawerTitle.value = "";
-  console.log("handleCreateDrawer");
+  isTitleTouched.value = false;
 }
 </script>
 <style scoped></style>
