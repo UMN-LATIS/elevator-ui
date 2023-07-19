948bb622e644474ce9caefd505ebbac8f5656b7c
James Johnson <johnsojr@umn.edu>
2023-07-19 15:06:09 -0500

    Add a logout page for handling logout stuff

---
+++ src/pages/LogoutPage/LogoutPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <DefaultLayout>
+    <h1>Logged out</h1>
+  </DefaultLayout>
+</template>
+<script setup lang="ts">
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import { onMounted } from "vue";
+import api from "@/api";
+
+onMounted(() => {
+  api.logout();
+
+  // do a full reload to clear any cache
+  window.location.href = "/";
+});
+</script>
+<style scoped></style>
