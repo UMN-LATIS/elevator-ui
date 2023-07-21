UMN-LATIS/elevator-ui

commit 17fdc62827853735b5c1ee1cebca90ab81ccf827

Clear search filters on static pages (#159)

src/stores/searchStore.ts
@@ -250,7 +250,9 @@ const actions = (state: SearchStoreState) => ({
     state.sort.value = option;
 
     // refresh search results
-    return this.search();
+    if (state.searchId.value) {
+      return this.search(state.searchId.value);
+    }
   },
 
   addCollectionIdFilter(collectionId: number) {
@@ -429,38 +431,27 @@ const actions = (state: SearchStoreState) => ({
     state.filterBy.searchableFieldsOperator = "AND";
   },
 
-  async search(searchId?: string): Promise<string | void> {
+  async getSearchId(): Promise<string> {
+    return api
+      .getSearchId(state.query.value, getters(state).searchRequestOptions.value)
+      .catch((err) => {
+        throw new Error(`Cannot getSearchId for query: ${state.query}: ${err}`);
+      });
+  },
+
+  async search(searchId: string): Promise<string | void> {
     // call all registered before handlers
     state.beforeNewSearchHandlers.forEach((fn) => fn());
 
     // clear old search results
     state.status.value = "fetching";
-    state.searchId.value = undefined;
+    state.searchId.value = searchId;
     state.matches.value = [];
     state.totalResults.value = undefined;
     state.currentPage.value = 0;
     state.searchEntry.value = null;
 
     try {
-      // first get the id of the search for this query
-      // if it's passed, use that
-      state.searchId.value = searchId
-        ? searchId
-        : await api
-            .getSearchId(
-              state.query.value,
-              getters(state).searchRequestOptions.value
-            )
-            .catch((err) => {
-              state.status.value = "error";
-              throw new Error(
-                `Cannot getSearchId for query: ${state.query}: ${err}`
-              );
-            });
-
-      // async (don't await) get search results and update store
-      // so that we can return the search id so they can redirect
-      // if needed
       api
         .getSearchResultsById(state.searchId.value)
         .then((res) => {

src/components/SearchBar/SearchBar.vue
@@ -34,14 +34,13 @@
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref, watch } from "vue";
+import { ref, watch } from "vue";
 import InputGroup from "@/components/InputGroup/InputGroup.vue";
 import AdvancedSearchForm from "@/components/AdvancedSearchForm/AdvancedSearchForm.vue";
 import { useSearchStore } from "@/stores/searchStore";
-import { useRouter } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
 import SearchTextInputGroup from "./SearchTextInputGroup.vue";
 import TransitionFade from "@/components/TransitionFade/TransitionFade.vue";
-import { useScrollLock } from "@vueuse/core";
 
 const inputGroup = ref<InstanceType<typeof InputGroup> | null>(null);
 const isAdvancedSearchModalOpen = ref(false);
@@ -52,7 +51,7 @@ async function handleSubmit() {
   // close advanced search modal if open
   isAdvancedSearchModalOpen.value = false;
 
-  const searchId = await searchStore.search();
+  const searchId = await searchStore.getSearchId();
   if (!searchId) {
     router.push({
       name: "error",
@@ -85,12 +84,23 @@ function removeFocusOnEscape(event: KeyboardEvent) {
 document.addEventListener("keydown", focusInputOnCommandK);
 document.addEventListener("keydown", removeFocusOnEscape);
 
-// setup body scroll lock when advanced search modal is open
-// const body = document.querySelector("body");
-// const isLocked = useScrollLock(body);
-// watch(isAdvancedSearchModalOpen, () => {
-//   isLocked.value = isAdvancedSearchModalOpen.value;
-// });
+const route = useRoute();
+
+watch(
+  route,
+  (to, from) => {
+    const isNewRoute = to.name !== from?.name;
+    const isNotSearchOrAssetPage = !["search", "asset"].includes(
+      to.name as string
+    );
+
+    if (isNewRoute && isNotSearchOrAssetPage) {
+      searchStore.query = "";
+      searchStore.clearAllFilters();
+    }
+  },
+  { immediate: true }
+);
 </script>
 <style scoped>
 @media (min-width: 640px) {

src/pages/SearchResultsPage/SearchResultsPage.vue
@@ -23,7 +23,7 @@
           >
             <ResultsCount class="mb-2 sm:mb-0" />
             <SearchResultsSortSelect
-              v-if="!['map', 'timline'].includes(searchStore.resultsView)"
+              v-if="!['map', 'timeline'].includes(searchStore.resultsView)"
               :sortOptions="searchStore.sortOptions"
               :selectedSortOption="searchStore.sort"
               :searchQuery="
@@ -92,7 +92,7 @@
 </template>
 <script setup lang="ts">
 import { watch, computed, onMounted, nextTick, ref } from "vue";
-import { useRouter, useRoute, onBeforeRouteLeave } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { useSearchStore } from "@/stores/searchStore";
 import BrowseCollectionHeader from "./BrowseCollectionHeader.vue";
@@ -144,17 +144,10 @@ searchStore.onAfterNewSearch(() => {
   isNewSearchReadyForDisplay.value = true;
 });
 
-// if search with this id is not currently in flight,
-// then kick it off
+// load search results
 watch(
   () => props.searchId,
-  () => {
-    if (searchStore.searchId === props.searchId) {
-      isNewSearchReadyForDisplay.value = true;
-      return;
-    }
-    searchStore.search(props.searchId);
-  },
+  () => searchStore.search(props.searchId),
   { immediate: true }
 );
 
@@ -251,12 +244,6 @@ watch(
   },
   { immediate: true }
 );
-
-// clear the search query when leaving this page
-onBeforeRouteLeave(() => {
-  searchStore.query = "";
-  searchStore.clearAllFilters();
-});
 </script>
 <style scoped></style>
 <style>

src/router.ts
@@ -8,6 +8,7 @@ import SearchResultsPage from "./pages/SearchResultsPage/SearchResultsPage.vue";
 import LocalLoginPage from "./pages/LocalLoginPage/LocalLoginPage.vue";
 import StaticContentPage from "@/pages/StaticContentPage/StaticContentPage.vue";
 import ErrorPage from "@/pages/ErrorPage/ErrorPage.vue";
+import { useSearchStore } from "./stores/searchStore";
 
 function parseIntFromParam(
   param: string | string[] | undefined
@@ -65,6 +66,7 @@ const router = createRouter({
       // import("@/pages/AllCollectionsPage/AllCollectionsPage.vue"),
     },
     {
+      name: "browseCollection",
       path: "/collections/browseCollection/:collectionId",
       alias: "/collections/:collectionId",
       component: BrowseCollectionPage,
@@ -96,7 +98,7 @@ const router = createRouter({
       }),
     },
     {
-      name: "StaticContentPage",
+      name: "staticContentPage",
       path: "/page/view/:pageId",
       component: StaticContentPage,
       // component: () =>
@@ -115,6 +117,7 @@ const router = createRouter({
       }),
     },
     {
+      name: "catchall",
       path: "/:pathMatch(.*)",
       component: ErrorPage,
       // component: () => import("@/pages/ErrorPage/ErrorPage.vue"),

commit b359f192feb63e096aca8e603d61df89334cd855

fix: search results don't update when sort changes
This fixes a regression (introduced in 17fdc62) where the results on the search page don't update after a user changes the sort order. After updating the searchStore's state with the new sort option, we need to get first get the new searchId, and then use the searchId to grab the results.

Previously, we weren't getting a new searchId after the sort changes.

src/stores/searchStore.ts
@@ -295,11 +295,11 @@ const getters = (state: SearchStoreState) => ({
 const actions = (state: SearchStoreState) => ({
   async setSortOption(option: keyof SearchSortOptions) {
     state.sort.value = option;
+    // get the search ID for this sort option
+    state.searchId.value = await this.getSearchId();
 
-    // refresh search results
-    if (state.searchId.value) {
-      return this.search(state.searchId.value);
-    }
+    // update search results
+    return this.search(state.searchId.value);
   },
 
   addCollectionIdFilter(collectionId: number) {