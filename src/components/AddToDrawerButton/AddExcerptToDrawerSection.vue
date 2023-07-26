ec4301b1c5188a2206863aa5f1d0236ec16bc38f
James Johnson <johnsojr@umn.edu>
2023-07-26 15:39:19 -0500

    WIP

--- src/components/AddToDrawerButton/AddExcerptToDrawerSection.vue
+++ src/components/AddToDrawerButton/AddExcerptToDrawerSection.vue
@@ -24,31 +24,47 @@
       <div class="flex gap-4 justify-center">
         <InputGroup
           id="excerpt__start-time"
-          :modelValue="startTimeString"
+          v-model="startTimeString"
           label="Start Time"
           placeholder="00:00"
           type="text"
           class="w-32"
-          @update:modelValue="handleStartTimeChange"
+          @blur="handleStartTimeChange"
         >
           <template #append>
-            <Button variant="tertiary" class="text-sm">Set</Button>
+            <Button
+              variant="tertiary"
+              class="text-sm"
+              @click="handleSetCurrentAsStartTime"
+              >Set</Button
+            >
           </template>
         </InputGroup>
         <InputGroup
           id="excerpt__end-time"
-          :modelValue="endTimeString"
+          v-model="endTimeString"
           label="End Time"
           placeholder="00:00"
           type="text"
           class="w-32"
-          @update:modelValue="handleEndTimeChange"
+          :class="{
+            'border-red-600': !isValidEndTime,
+          }"
+          @blur="handleEndTimeChange"
         >
           <template #append>
-            <Button variant="tertiary" class="text-sm">Set</Button>
+            <Button
+              variant="tertiary"
+              class="text-sm"
+              @click="handleSetCurrentAsEndTime"
+              >Set</Button
+            >
           </template>
         </InputGroup>
       </div>
+      <p v-if="!isValidEndTime" class="text-red-600 text-xs italic text-center">
+        End time must be after start time.
+      </p>
     </div>
   </div>
 </template>
@@ -57,6 +73,7 @@ import { ref, computed } from "vue";
 import InputGroup from "@/components/InputGroup/InputGroup.vue";
 import Button from "@/components/Button/Button.vue";
 import ObjectViewer from "@/components/ObjectViewer/ObjectViewer.vue";
+import config from "@/config";
 
 export interface Excerpt {
   startTime: number | null;
@@ -73,27 +90,124 @@ const emit = defineEmits<{
 }>();
 
 const isAddingExcerpt = ref(false);
-const startTimeString = computed(() => {
-  return props.modelValue.startTime?.toString() ?? "";
-});
-const endTimeString = computed(() => {
-  return props.modelValue.endTime?.toString() ?? "";
+const currentScrubberPosition = ref(0);
+const endTimeString = ref("");
+const startTimeString = ref("");
+
+function isValidTimeString(timeString: string) {
+  const parts = timeString.split(":");
+  if (parts.length < 0 || parts.length > 3) return false;
+  for (const part of parts) {
+    const partAsNumber = Number.parseInt(part);
+
+    // I mean, I guess the number of hours could be greater than 60, but I'm
+    // going to assume that's not the case for now.
+    if (Number.isNaN(partAsNumber) || partAsNumber < 0 || partAsNumber >= 60) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function secondsToTimeString(seconds: number | null) {
+  if (seconds === null) return "";
+
+  if (Number.isNaN(seconds)) {
+    throw new Error("Cannot convert seconds to time string: seconds is NaN");
+  }
+
+  if (seconds >= 60 * 60 * 24) {
+    throw new Error(
+      "Cannot convert seconds to time string: seconds larger than 1 day"
+    );
+  }
+
+  if (seconds < 60 * 60) {
+    // MM:SS
+    return new Date(seconds * 1000).toISOString().slice(14, 19);
+  }
+
+  // HH:MM:SS
+  return new Date(seconds * 1000).toISOString().slice(11, 19);
+}
+
+function timeStringToSeconds(timeString: string): number | null {
+  if (!isValidTimeString(timeString)) {
+    return null;
+  }
+
+  // 12:34:56 => [56, 34, 12]
+  const parts = timeString.split(":").reverse();
+
+  let seconds = 0;
+  parts.forEach((part, index) => {
+    // 12:34:56 => 56 * 60^0 + 34 * 60^1 + 12 * 60^2
+    seconds += Number.parseInt(part) * Math.pow(60, index);
+  });
+  return seconds;
+}
+
+// check that startTime < endTime
+const isValidEndTime = computed(() => {
+  const startTime = timeStringToSeconds(startTimeString.value);
+  const endTime = timeStringToSeconds(endTimeString.value);
+
+  return startTime === null || endTime === null || endTime > startTime;
 });
 
-function handleEndTimeChange(value: string) {
-  const endTime = Number.parseInt(value);
+function handleEndTimeChange() {
+  const startTime = timeStringToSeconds(startTimeString.value);
   emit("update:modelValue", {
     ...props.modelValue,
-    endTime: Number.isNaN(endTime) ? null : endTime,
+    endTime: timeStringToSeconds(endTimeString.value),
   });
 }
 
-function handleStartTimeChange(value: string) {
-  const startTime = Number.parseInt(value);
+function handleStartTimeChange() {
   emit("update:modelValue", {
     ...props.modelValue,
-    startTime: Number.isNaN(startTime) ? null : startTime,
+    startTime: timeStringToSeconds(startTimeString.value),
   });
 }
+
+function handleSetCurrentAsEndTime() {
+  endTimeString.value = secondsToTimeString(currentScrubberPosition.value);
+  handleEndTimeChange();
+}
+
+function handleSetCurrentAsStartTime() {
+  startTimeString.value = secondsToTimeString(currentScrubberPosition.value);
+  handleStartTimeChange();
+}
+
+interface ScrubberUpdateMessageEvent extends MessageEvent {
+  data: {
+    type: "pause" | "seeked";
+    currentPosition: number; // seconds from start
+  };
+}
+
+const allowedMessageEventOriginPatterns = [
+  // "https?:\/\/localhost:\\d+",
+  // "https?:\/\/.*\.elevator\.umn.\.edu",
+  config.instance.base.origin,
+  window.location.origin,
+];
+const allowedMessageEventOriginsRegex = new RegExp(
+  `^(${allowedMessageEventOriginPatterns.join("|")})$`
+);
+
+window.addEventListener("message", (event: ScrubberUpdateMessageEvent) => {
+  // If invalid origin, discard the message
+  if (!allowedMessageEventOriginsRegex.test(event.origin)) {
+    throw new Error(
+      `Cannot update current scruber position. The MessageEvent is from invalid origin: ${event.origin}`
+    );
+  }
+
+  // update current scrubber position
+  currentScrubberPosition.value = event.data.currentPosition;
+  console.log("scrubber position updated", currentScrubberPosition.value);
+});
 </script>
 <style scoped></style>
--- src/components/Popover/Popover.vue
+++ src/components/Popover/Popover.vue
@@ -1,15 +1,12 @@
 <template>
   <Popper class="popover" :hover="hover" :arrow="arrow">
     <slot />
-    <template v-if="!content" #content>
-      <div class="popover-content text-xs py-1 px-2">
-        <slot name="content" />
-      </div>
-    </template>
-    <template v-else #content>
-      <span class="text-xs py-1 px-2 whitespace-nowrap" v-bind="$attrs">{{
-        content
-      }}</span>
+    <template #content>
+      <slot name="content">
+        <div class="popover-content" v-bind="$attrs">
+          {{ content }}
+        </div>
+      </slot>
     </template>
   </Popper>
 </template>
@@ -31,16 +28,20 @@ withDefaults(
 </script>
 <style>
 :root {
-  --popper-theme-background-color: var(--app-background-color, #fff);
+  --popper-theme-background-color: var(--app-background-color, #111);
   --popper-theme-background-color-hover: var(
     --app-background-color-hover,
-    #fff
+    #111
   );
-  --popper-theme-text-color: var(--app-text-color, #111);
+  --popper-theme-text-color: var(--app-text-color, #ccc);
   --popper-theme-border-width: 0px;
   --popper-theme-border-style: solid;
-  /* --popper-theme-border-radius: 0.25rem; */
-  /* --popper-theme-padding: 0.5rem; */
+  --popper-theme-border-radius: 0.25rem;
+  --popper-theme-padding: 0.5em;
   --popper-theme-box-shadow: 0 6px 30px -6px rgba(0, 0, 0, 0.25);
+}
+
+.popover-content {
+  font-size: 14px;
 }
 </style>
