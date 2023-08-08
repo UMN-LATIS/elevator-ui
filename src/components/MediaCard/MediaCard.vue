f5f5b88f819c0ed721d339a8e087a49486c94182
James Johnson <johnsojr@umn.edu>
2023-08-08 13:42:54 -0500

    fix media card focus

--- src/components/MediaCard/MediaCard.vue
+++ src/components/MediaCard/MediaCard.vue
@@ -1,33 +1,31 @@
 <template>
-  <article
-    class="media-card flex flex-col overflow-hidden border rounded-md shadow-sm"
+  <component
+    :is="to ? Link : 'div'"
+    :to="to"
+    class="media-card hover:no-underline group border rounded-md shadow-sm overflow-hidden opacity-75 focus:opacity-100 hover:opacity-100 hover:shadow-lg focus:shadow-lg"
   >
-    <div class="placeholder-image aspect-video overflow-hidden">
-      <component
-        :is="to ? Link : 'div'"
-        :to="to"
-        class="flex items-center justify-center w-full h-full media-card__image"
-      >
-        <LazyLoadImage
-          v-if="imgSrc"
-          :src="imgSrc"
-          :alt="imgAlt || 'Untitled'"
-          loading="lazy"
-          class="object-cover w-full h-full"
-        />
-        <DocumentIcon v-else />
-      </component>
-    </div>
+    <article class="flex flex-col">
+      <div class="placeholder-image aspect-video overflow-hidden">
+        <div
+          class="flex items-center justify-center w-full h-full media-card__image"
+        >
+          <LazyLoadImage
+            v-if="imgSrc"
+            :src="imgSrc"
+            :alt="imgAlt || 'Untitled'"
+            loading="lazy"
+            class="object-cover w-full h-full"
+          />
+          <DocumentIcon v-else />
+        </div>
+      </div>
 
-    <component
-      :is="to ? Link : 'div'"
-      :to="to"
-      class="flex-1 p-4 media-card__body hover:no-underline"
-    >
-      <slot />
-    </component>
-    <slot name="footer"></slot>
-  </article>
+      <div :to="to" class="flex-1 p-4 media-card__body hover:no-underline">
+        <slot />
+      </div>
+      <slot name="footer"></slot>
+    </article>
+  </component>
 </template>
 <script setup lang="ts">
 import { DocumentIcon } from "@/icons";
@@ -58,7 +56,8 @@ defineProps<{
 }
 
 .media-card:has(.media-card__image:hover),
-.media-card:has(.media-card__body:hover) {
+.media-card:has(.media-card__body:hover),
+.media-card:focus {
   --hover-text-color: var(--color-blue-600);
   --hover-bg-color: var(--color-blue-50);
   background: var(--hover-bg-color);
