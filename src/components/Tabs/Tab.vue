cd78d44fb711d90ee8e921e2b80a4d455964752f
James Johnson <johnsojr@umn.edu>
2023-10-17 23:18:00 -0500

    default to v-show

--- src/components/Tabs/Tab.vue
+++ src/components/Tabs/Tab.vue
@@ -1,20 +1,32 @@
 <template>
-  <div v-if="tabsContext?.isActiveTab(id)">
+  <div v-if="isActiveTab || !unmountWhenInactive" v-show="isActiveTab">
     <slot></slot>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { inject, onMounted, onUnmounted } from "vue";
+import { inject, onMounted, onUnmounted, computed } from "vue";
 import { TabsInjectionKey } from "@/constants/constants";
 import type { TabsContext } from "@/types";
 
-const props = defineProps<{
-  id: string;
-  label: string;
-}>();
+const props = withDefaults(
+  defineProps<{
+    id: string;
+    label: string;
+    unmountWhenInactive?: boolean;
+  }>(),
+  {
+    unmountWhenInactive: false,
+  }
+);
 
 const tabsContext = inject<TabsContext>(TabsInjectionKey);
+const isActiveTab = computed(() => {
+  if (!tabsContext) {
+    throw new Error("TabsContext not found");
+  }
+  return tabsContext.isActiveTab(props.id);
+});
 
 onMounted(() => {
   if (!tabsContext) {
