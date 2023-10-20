8dd1b5954f933560a03e2b167dae05f1b9fdcad2
James Johnson <johnsojr@umn.edu>
2023-10-20 14:33:42 -0500

    Add some helpers for editing static content pages (#253)
    Co-authored-by: Elevator Dev (ubuntu) <latistecharch@umn.edu>

--- src/pages/StaticContentPage/StaticContentPage.vue
+++ src/pages/StaticContentPage/StaticContentPage.vue
@@ -5,12 +5,18 @@
     </template>
     <div
       v-if="page"
-      class="static-page__content p-4 lg:p-8 mx-auto flex-1 w-full max-w-screen-xl"
-    >
-      <div class="prose prose-neutral">
-        <h1 class="text-4xl font-bold">
-          {{ page.title || "No Title" }}
+      class="static-page__content p-4 lg:p-8 mx-auto flex-1 w-full max-w-screen-xl">
+      <a
+        v-if="canCurrentUserEdit"
+        :href="`${BASE_URL}/instances/editPage/${pageId}`"
+        class="float-right uppercase text-xs font-medium bg-blue-100 px-2 py-1 rounded-md no-underline hover:bg-blue-600 hover:text-blue-100 hover:no-underline">
+        Edit Page
+      </a>
+      <div class="prose prose-neutral mx-auto">
+        <h1 class="text-4xl font-bold text-center">
+          {{ page.title || "Untitled" }}
         </h1>
+
         <SanitizedHTML :html="page.content ?? ''" class="w-full" />
       </div>
     </div>
@@ -24,10 +30,11 @@ import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import CustomAppHeader from "@/components/CustomAppHeader/CustomAppHeader.vue";
 import SanitizedHTML from "@/components/SanitizedHTML/SanitizedHTML.vue";
 import AppFooter from "@/components/AppFooter/AppFooter.vue";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import { ApiStaticPageResponse } from "@/types";
 import { useInstanceStore } from "@/stores/instanceStore";
 import api from "@/api";
+import config from "@/config";
 
 const instanceStore = useInstanceStore();
 
@@ -35,7 +42,15 @@ const props = defineProps<{
   pageId: number;
 }>();
 
+const BASE_URL = config.instance.base.path;
 const page = ref<ApiStaticPageResponse | null>(null);
+
+const canCurrentUserEdit = computed(() => {
+  return (
+    instanceStore.currentUser?.isAdmin ||
+    instanceStore.currentUser?.isSuperAdmin
+  );
+});
 
 watch(
   () => props.pageId,
