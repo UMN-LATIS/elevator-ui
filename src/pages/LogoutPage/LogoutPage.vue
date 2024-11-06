68aff37f25871406c25c08efbb1e4ccfffad47b7
James Johnson <johnsojr@umn.edu>
2024-11-06 12:21:44 -0600

    remove `_check_is_passive` cookie on logout

--- src/pages/LogoutPage/LogoutPage.vue
+++ src/pages/LogoutPage/LogoutPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
+import Cookies from "js-cookie";
 import api from "@/api";
 import { useRouter } from "vue-router";
 import { resetAllStores } from "@/stores/resetAllStores";
@@ -13,9 +14,14 @@ const router = useRouter();
 onMounted(async () => {
   await api.logout();
 
+  // clear cookie indicating active session
+  // see: https://github.com/UMN-LATIS/elevator-ui/issues/277
+  Cookies.remove("_check_is_passive");
+
   // do a full reload to clear any cached state
   api.clearCache();
   resetAllStores();
+
   instanceStore.init();
   drawerStore.init();
   router.push("/");
