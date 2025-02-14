f22f443e42631eefa1dab05a700d4b16dbd7701e
James Johnson <johnsojr@umn.edu>
2025-02-13 23:17:00 -0600

    better handling of long app names (#286)

--- src/components/AppLogoMark/AppLogoMark.vue
+++ src/components/AppLogoMark/AppLogoMark.vue
@@ -6,7 +6,8 @@
       :alt="instanceStore.instance.logoImg.alt"
       class="app-header__logo-img h-8" />
     <ElevatorIcon v-else class="h-full app-header__icon" />
-    <h1 class="app-header__wordmark font-bold text-lg hidden md:block">
+    <h1
+      class="app-header__wordmark font-bold text-lg hidden md:block sm:max-w-[16rem] lg:max-w-[32rem] overflow-hidden overflow-ellipsis">
       {{ instanceStore.instance.name }}
     </h1>
   </div>
