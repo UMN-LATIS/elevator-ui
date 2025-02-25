8a19719a990bc8c872da5c83ba5530c94e31802e
James Johnson <johnsojr@umn.edu>
2025-02-25 15:30:12 -0600

    cleanup

--- src/stores/assetStore.ts
+++ src/stores/assetStore.ts
@@ -42,10 +42,6 @@ export const useAssetStore = defineStore("asset2", {
       // track this asset view
       const { trackEvent } = useAnalytics();
       trackEvent("asset_view", {
-        // QUESTION
-        // there's a cap on number of custom dimensions. Should we use built in dimensions like `contet_group` instead?
-        // see: https://support.google.com/analytics/table/13948007?visit_id=638761100108506149-1634441230&rd=2
-        key_event: true,
         collection_id: asset.collectionId,
         collection_name: asset.collectionName,
         asset_title: getAssetTitle(asset),
