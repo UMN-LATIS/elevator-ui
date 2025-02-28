805c029c0687447b2620cd32123acf678f6ba3ee
James Johnson <johnsojr@umn.edu>
2025-02-28 16:21:58 -0600

    wip download all button

--- src/components/Widget/UploadWidget/UploadWidget.vue
+++ src/components/Widget/UploadWidget/UploadWidget.vue
@@ -1,31 +1,33 @@
 <template>
-  <div class="upload-widget flex gap-2 flex-wrap mt-2">
-    <button
-      v-for="(content, key) in contents"
-      :key="key"
-      :title="content.fileDescription"
-      class="thumbnail-related-asset-widget flex flex-col rounded-md border border-transparent p-1 no-underline hover:no-underline hover:bg-blue-50 hover:text-blue-600 w-24 text-neutral-600"
-      :class="{
-        'opacity-80 hover:opacity-100 hover:border-blue-600': !isFileActive(
-          content.fileId
-        ),
-        'ring ring-offset-1 ring-blue-600 hover:border-transparent opacity-100 bg-blue-50':
-          isFileActive(content.fileId),
-      }"
-      @click="assetStore.activeFileObjectId = content.fileId"
-    >
-      <ThumbnailImage
-        :src="`${config.instance.base.url}/fileManager/tinyImageByFileId/${content.fileId}/true`"
-        :alt="content.fileDescription"
-        :fileType="content.fileType"
-        class="thumbnail-related-asset-widget__image max-w-full"
-      />
-      <SanitizedHTML
-        v-if="content.fileDescription"
-        class="whitespace-nowrap text-xs mt-1 truncate overflow-hidden w-full text-center"
-        :html="content.fileDescription"
-      />
-    </button>
+  <div>
+    <Button variant="tertiary" class="-ml-2" @click="handleDownloadAllFiles">
+      Download All Files
+    </Button>
+    <div class="upload-widget flex gap-2 flex-wrap mt-2">
+      <button
+        v-for="(content, key) in contents"
+        :key="key"
+        :title="content.fileDescription"
+        class="thumbnail-related-asset-widget flex flex-col rounded-md border border-transparent p-1 no-underline hover:no-underline hover:bg-blue-50 hover:text-blue-600 w-24 text-neutral-600"
+        :class="{
+          'opacity-80 hover:opacity-100 hover:border-blue-600': !isFileActive(
+            content.fileId
+          ),
+          'ring ring-offset-1 ring-blue-600 hover:border-transparent opacity-100 bg-blue-50':
+            isFileActive(content.fileId),
+        }"
+        @click="assetStore.activeFileObjectId = content.fileId">
+        <ThumbnailImage
+          :src="`${config.instance.base.url}/fileManager/tinyImageByFileId/${content.fileId}/true`"
+          :alt="content.fileDescription"
+          :fileType="content.fileType"
+          class="thumbnail-related-asset-widget__image max-w-full" />
+        <SanitizedHTML
+          v-if="content.fileDescription"
+          class="whitespace-nowrap text-xs mt-1 truncate overflow-hidden w-full text-center"
+          :html="content.fileDescription" />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -36,6 +38,8 @@ import { useAssetStore } from "@/stores/assetStore";
 import ThumbnailImage from "@/components/ThumbnailImage/ThumbnailImage.vue";
 import SanitizedHTML from "@/components/SanitizedHTML/SanitizedHTML.vue";
 import { computed, onMounted, onBeforeUnmount } from "vue";
+import Button from "@/components/Button/Button.vue";
+import api from "@/api";
 
 const props = defineProps<{
   widget: UploadWidgetProps;
@@ -76,6 +80,42 @@ function handleNextPrevArrowPresses(event: KeyboardEvent) {
   if (event.key === "ArrowRight") {
     return setNextFileAsActive();
   }
+}
+
+async function downloadFile(fileObjectId, assetId) {
+  console.log("Download file", fileObjectId);
+  const downloadInfo = await api.getFileDownloadInfo(fileObjectId, assetId);
+
+  // the first one in downloadInfo is the preferred download.
+  const preferredDownload = downloadInfo?.[0] ?? null;
+
+  if (!preferredDownload) {
+    console.error("No download info found for file", fileObjectId);
+    return;
+  }
+
+  try {
+    const link = document.createElement("a");
+    link.href = preferredDownload.url;
+    link.download = `${preferredDownload.originalFilename}-${assetId}-${fileObjectId}-${preferredDownload.filetype}.${preferredDownload.extension}`;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  } catch (error) {
+    console.error("Error downloading file", error);
+    throw error;
+  }
+}
+
+async function handleDownloadAllFiles() {
+  // snapshot the assetId in case it changes during the download process
+  const assetId = assetStore.activeAssetId;
+
+  const promisedDownloads = props.contents.map((content) =>
+    downloadFile(content.fileId, assetId)
+  );
+
+  return Promise.allSettled(promisedDownloads);
 }
 
 onMounted(() => {
