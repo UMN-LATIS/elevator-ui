482306cf8f28df047fc7360fa3dae5bfabe2f064
James Johnson <johnsojr@umn.edu>
2025-04-08 12:21:31 -0500

    stub edit asset page

---
+++ src/pages/EditAssetPage/EditAssetPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <DefaultLayout>
+    <div class="edit-asset-page">
+      <h1>Edit Asset Page</h1>
+      <p>This is the edit asset page.</p>
+    </div>
+  </DefaultLayout>
+</template>
+<script setup lang="ts">
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+</script>
+<style scoped></style>
