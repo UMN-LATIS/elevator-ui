120a6598fcc1763c746b9fc240c2c99bb5fb133d
James Johnson <johnsojr@umn.edu>
2025-04-08 13:15:57 -0500

    create default widget content  by type

---
+++ src/helpers/createDefaultWidgetContents.ts
@@ -0,0 +1,74 @@
+import { WIDGET_TYPES, WidgetProps } from "@/types";
+
+export function createDefaultWidgetContent(widgetProps: WidgetProps) {
+  const base = {
+    isPrimary: false,
+    id: crypto.randomUUID(),
+  };
+
+  switch (widgetProps.type) {
+    case WIDGET_TYPES.TEXT:
+      return {
+        ...base,
+        fieldContents: "",
+      };
+    case WIDGET_TYPES.TEXT_AREA:
+      return {
+        ...base,
+        fieldContents: null,
+      };
+    case WIDGET_TYPES.CHECKBOX:
+      return {
+        ...base,
+        fieldContents: false,
+      };
+    case WIDGET_TYPES.DATE:
+      return {
+        ...base,
+        label: "",
+        start: { text: null, numeric: null },
+        end: { text: null, numeric: null },
+      };
+    case WIDGET_TYPES.LOCATION:
+      return {
+        ...base,
+        locationLabel: null,
+        address: null,
+        loc: null,
+      };
+    case WIDGET_TYPES.TAG_LIST:
+      return {
+        ...base,
+        tags: null,
+      };
+    case WIDGET_TYPES.SELECT:
+      return {
+        ...base,
+        fieldContents: {},
+      };
+    case WIDGET_TYPES.MULTISELECT:
+      return {
+        ...base,
+        fieldContents: {},
+      };
+    case WIDGET_TYPES.RELATED_ASSET:
+      return {
+        ...base,
+        targetAssetId: null,
+        label: null,
+      };
+    case WIDGET_TYPES.UPLOAD:
+      return {
+        ...base,
+        fileId: "",
+        fileDescription: "",
+        fileType: "",
+        searchData: null,
+        loc: null,
+        sidecars: {},
+      };
+    default:
+      const exhaustiveCheck: never = widgetProps.type;
+      throw new Error(`Unhandled widget type: ${exhaustiveCheck}`);
+  }
+}
