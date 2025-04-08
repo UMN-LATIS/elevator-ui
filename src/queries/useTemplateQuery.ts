589966c3b73d87c96ff31469ea6dff97ea45093d
James Johnson <johnsojr@umn.edu>
2025-04-08 13:02:04 -0500

    cleanup

--- src/queries/useTemplateQuery.ts
+++ src/queries/useTemplateQuery.ts
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/vue-query";
 import * as fetchers from "@/api/fetchers";
 import { toValue, type MaybeRefOrGetter } from "vue";
 import { TEMPLATES_QUERY_KEY } from "./queryKeys";
-import type { Template } from "@/types";
 
 export function useTemplateQuery(
   templateId: MaybeRefOrGetter<string | number | null>,
@@ -14,7 +13,6 @@ export function useTemplateQuery(
     enabled: () => !!toValue(templateId),
     initialData: () => null,
     queryFn: async () => {
-      console.log("running template query");
       const id = toValue(templateId);
       const idInt = Number.parseInt(id as string);
       return id ? await fetchers.fetchTemplate(idInt) : null;
