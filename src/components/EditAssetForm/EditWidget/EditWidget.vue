10ee0f20869bf127592a3e6d1ac91f4ee1ba0fb5
James Johnson <johnsojr@umn.edu>
2025-04-08 13:24:02 -0500

    add editwidget component to dispatch to specific widget type

---
+++ src/components/EditAssetForm/EditWidget/EditWidget.vue
@@ -0,0 +1,45 @@
+<template>
+  <component
+    :is="getWidgetComponentByType(widgetDef.type)"
+    :widgetDef="widgetDef"
+    :widgetContents="widgetContents" />
+</template>
+<script setup lang="ts">
+import { type Component } from "vue";
+import type { WidgetProps, WidgetContent } from "@/types";
+import { WidgetType } from "@/types";
+import EditSelectWidget from "./EditSelectWidget.vue";
+import EditCheckboxWidget from "./EditCheckboxWidget.vue";
+import EditTextAreaWidget from "./EditTextAreaWidget.vue";
+import EditDateWidget from "./EditDateWidget.vue";
+import EditMultiSelectWidget from "./EditMultiSelectWidget.vue";
+import EditLocationWidget from "./EditLocationWidget.vue";
+import EditUploadWidget from "./EditUploadWidget.vue";
+import EditTagWidget from "./EditTagWidget.vue";
+import EditRelatedAssetWidget from "./EditRelatedAssetWidget.vue";
+import EditTextWidget from "./EditTextWidget.vue";
+
+defineProps<{
+  widgetDef: WidgetProps;
+  widgetContents: WidgetContent[];
+}>();
+
+// map widgetTypeToComponent
+const widgetMap: Record<WidgetType, Component> = {
+  text: EditTextWidget,
+  select: EditSelectWidget,
+  checkbox: EditCheckboxWidget,
+  "text area": EditTextAreaWidget,
+  date: EditDateWidget,
+  multiselect: EditMultiSelectWidget,
+  location: EditLocationWidget,
+  upload: EditUploadWidget,
+  "tag list": EditTagWidget,
+  "related asset": EditRelatedAssetWidget,
+};
+
+function getWidgetComponentByType(type: string) {
+  return widgetMap[type] || null;
+}
+</script>
+<style scoped></style>
