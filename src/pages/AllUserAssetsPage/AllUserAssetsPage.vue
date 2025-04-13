9ddee10a712e0aa90ad18219fbe4ff5eeffe53e9
James Johnson <johnsojr@umn.edu>
2025-04-12 19:45:13 -0600

    add Add Asset button to User Assets page

--- src/pages/AllUserAssetsPage/AllUserAssetsPage.vue
+++ src/pages/AllUserAssetsPage/AllUserAssetsPage.vue
@@ -1,7 +1,12 @@
 <template>
   <DefaultLayout class="all-collections-page">
     <div class="container py-10 mx-auto">
-      <h1 class="text-4xl font-bold my-8">All My Assets</h1>
+      <div class="flex justify-between items-center">
+        <h1 class="text-4xl font-bold my-8">All My Assets</h1>
+        <RouterLink :to="{ name: 'addAsset' }" asChild>
+          <Button variant="primary" class="mb-4">Add Asset</Button>
+        </RouterLink>
+      </div>
       <p v-if="!allUserAssets.length" class="text-lg">No assets found.</p>
       <UserAssetsTable v-else :columns="columns" :data="allUserAssets" />
     </div>
@@ -10,6 +15,7 @@
 <script setup lang="ts">
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { useAllUserAssets } from "@/queries/useAllUserAssets";
+import Button from "@/components/Button/Button.vue";
 import { columns } from "./UserAssetsTableColumns";
 import UserAssetsTable from "./UserAssetsTable.vue";
 const { data: allUserAssets } = useAllUserAssets();
