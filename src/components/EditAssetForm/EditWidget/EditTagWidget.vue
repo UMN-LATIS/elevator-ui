e2dfdf4b9b56cb42f873512d2cfb1642c9360df4
James Johnson <johnsojr@umn.edu>
2025-04-18 13:09:23 -0700

    wip edit tag widget

--- src/components/EditAssetForm/EditWidget/EditTagWidget.vue
+++ src/components/EditAssetForm/EditWidget/EditTagWidget.vue
@@ -1,5 +1,77 @@
 <template>
-  <div class="">Edit Tag Widget</div>
+  <EditWidgetLayout
+    :widgetContents="widgetContents"
+    :widgetDef="widgetDef"
+    class="edit-tag-widget"
+    @add="handleAdd"
+    @setPrimary="handleSetPrimary"
+    @delete="handleDelete"
+    @update:widgetContents="updateWidgetContents">
+    <template #fieldContents="{ item }">
+      <TagsInput
+        :modelValue="item.tags ?? []"
+        class="tags-input"
+        @update:modelValue="console.log">
+        <TagsInputItem v-for="tag in item.tags" :key="tag" :value="tag">
+          <TagsInputItemText />
+          <TagsInputItemDelete />
+        </TagsInputItem>
+
+        <TagsInputInput :placeholder="`${widgetDef.label}...`" />
+      </TagsInput>
+    </template>
+  </EditWidgetLayout>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import * as Type from "@/types";
+import EditWidgetLayout from "./EditWidgetLayout.vue";
+import * as ops from "../editWidgetOps";
+import {
+  TagsInput,
+  TagsInputItem,
+  TagsInputItemText,
+  TagsInputItemDelete,
+  TagsInputInput,
+} from "@/components/ui/tags-input";
+
+// TODO: allow multiple, probably just means multiple TAGS, not multiple fields?
+
+const props = defineProps<{
+  widgetDef: Type.TagListWidgetProps;
+  widgetContents: Type.WithId<Type.TagListWidgetContent>[];
+}>();
+
+const emit = defineEmits<{
+  (
+    e: "update:widgetContents",
+    widgetContents: Type.WithId<Type.TagListWidgetContent>[]
+  ): void;
+}>();
+
+const updateWidgetContents = (
+  contents: Type.WithId<Type.TagListWidgetContent>[]
+) => emit("update:widgetContents", contents);
+
+const handleAdd = () =>
+  updateWidgetContents(
+    ops.addWidgetContent(props.widgetContents, props.widgetDef)
+  );
+
+const handleSetPrimary = (id: string) =>
+  updateWidgetContents(ops.setPrimaryItem(props.widgetContents, id));
+
+const handleDelete = (id: string) =>
+  updateWidgetContents(ops.deleteWidgetContent(props.widgetContents, id));
+
+const handleFieldUpdate = (
+  itemId: string,
+  fieldContents: Type.WithId<Type.TagListWidgetContent["tags"]>
+) => {
+  updateWidgetContents(
+    ops.updateWidgetContentItem(props.widgetContents, itemId, fieldContents)
+  );
+};
+</script>
+
 <style scoped></style>
--- src/components/ui/tags-input/TagsInput.vue
+++ src/components/ui/tags-input/TagsInput.vue
@@ -1,22 +1,45 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
-import { TagsInputRoot, type TagsInputRootEmits, type TagsInputRootProps, useForwardPropsEmits } from 'reka-ui'
-import { computed, type HTMLAttributes } from 'vue'
+import { cn } from "@/lib/utils";
+import {
+  TagsInputRoot,
+  type TagsInputRootEmits,
+  type TagsInputRootProps,
+  useForwardPropsEmits,
+} from "reka-ui";
+import { computed, type HTMLAttributes } from "vue";
 
-const props = defineProps<TagsInputRootProps & { class?: HTMLAttributes['class'] }>()
-const emits = defineEmits<TagsInputRootEmits>()
+const props = defineProps<
+  TagsInputRootProps & { class?: HTMLAttributes["class"] }
+>();
+const emits = defineEmits<TagsInputRootEmits>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+  const { class: _, ...delegated } = props;
 
-  return delegated
-})
+  return delegated;
+});
 
-const forwarded = useForwardPropsEmits(delegatedProps, emits)
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
 </script>
 
 <template>
-  <TagsInputRoot v-bind="forwarded" :class="cn('flex flex-wrap gap-2 items-center rounded-md border border-input bg-background px-3 py-1.5 text-sm', props.class)">
+  <TagsInputRoot
+    v-bind="forwarded"
+    :class="
+      cn(
+        'tags-input-root flex flex-wrap gap-2 items-center rounded-md border border-input bg-black/5 px-3 py-1.5 text-sm focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 focus-within:ring-blue-600',
+        props.class
+      )
+    ">
     <slot />
   </TagsInputRoot>
 </template>
+<style>
+.tags-input-root input {
+  border: none;
+}
+.tags-input-root input:focus {
+  border: none;
+  box-shadow: none;
+}
+</style>
