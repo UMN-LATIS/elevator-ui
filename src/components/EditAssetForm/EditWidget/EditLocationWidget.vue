8d15603d2867e2156eaf9248e3dabac4ba898f02
James Johnson <johnsojr@umn.edu>
2025-04-18 22:40:50 -0700

    wip location widget

--- src/components/EditAssetForm/EditWidget/EditLocationWidget.vue
+++ src/components/EditAssetForm/EditWidget/EditLocationWidget.vue
@@ -1,5 +1,78 @@
 <template>
-  <div class="">Edit Location Widget</div>
+  <EditWidgetLayout
+    :widgetContents="widgetContents"
+    :widgetDef="widgetDef"
+    class="edit-date-widget"
+    @add="
+      $emit(
+        'update:widgetContents',
+        ops.makeAddContentPayload(widgetContents, widgetDef)
+      )
+    "
+    @setPrimary="
+      (id) =>
+        $emit(
+          'update:widgetContents',
+          ops.makeSetPrimaryContentPayload(widgetContents, id)
+        )
+    "
+    @delete="
+      (id) =>
+        $emit(
+          'update:widgetContents',
+          ops.deleteWidgetContent(widgetContents, id)
+        )
+    "
+    @update:widgetContents="
+      (widgetContents) => {
+        $emit('update:widgetContents', widgetContents);
+      }
+    ">
+    <template #fieldContents="{ item }">
+      <EditLocationWidgetContentItem
+        :modelValue="item"
+        @update:modelValue="handleItemUpdate" />
+    </template>
+  </EditWidgetLayout>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as Type from "@/types";
+import EditWidgetLayout from "./EditWidgetLayout.vue";
+import * as ops from "../editWidgetOps";
+import EditLocationWidgetContentItem from "./EditLocationWidgetContentItem.vue";
+
+const props = defineProps<{
+  widgetDef: Type.LocationWidgetProps;
+  widgetContents: Type.WithId<Type.LocationWidgetContent>[];
+}>();
+
+const emit = defineEmits<{
+  (
+    e: "update:widgetContents",
+    widgetContents: Type.WithId<Type.LocationWidgetContent>[]
+  ): void;
+}>();
+
+function handleItemUpdate(
+  updatedItem: Type.WithId<Type.LocationWidgetContent>
+) {
+  console.log("handleItemUpdate", updatedItem);
+
+  const index = props.widgetContents.findIndex((i) => i.id === updatedItem.id);
+  if (index === -1) {
+    throw Error(
+      `Cannot update location widget: item with id "${updatedItem.id}" not found`
+    );
+  }
+
+  const updatedContents = [
+    ...props.widgetContents.slice(0, index),
+    updatedItem,
+    ...props.widgetContents.slice(index + 1),
+  ];
+
+  emit("update:widgetContents", updatedContents);
+}
+</script>
 <style scoped></style>
+<style></style>
---
+++ src/components/EditAssetForm/EditWidget/EditLocationWidgetContentItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    {{ props.modelValue }}
+  </div>
+</template>
+<script setup lang="ts">
+import * as Type from "@/types";
+
+const props = defineProps<{
+  modelValue: Type.WithId<Type.LocationWidgetContent>;
+}>();
+
+const emit = defineEmits<{
+  (
+    e: "update:modelValue",
+    contentItem: Type.WithId<Type.LocationWidgetContent>
+  ): void;
+}>();
+</script>
+<style scoped></style>
