d2625a9e936936681148574e0f90420999f0d73d
James Johnson <johnsojr@umn.edu>
2025-04-20 04:29:45 -0700

    stubs for related asset and upload widget

--- src/components/EditAssetForm/EditWidget/EditRelatedAssetWidget.vue
+++ src/components/EditAssetForm/EditWidget/EditRelatedAssetWidget.vue
@@ -1,5 +1,70 @@
 <template>
-  <div class="">Edit Related Asset Widget</div>
+  <EditWidgetLayout
+    :widgetContents="widgetContents"
+    :widgetDef="widgetDef"
+    class="edit-relatedasset-widget"
+    @add="
+      $emit(
+        'update:widgetContents',
+        ops.makeAddContentPayload(widgetContents, widgetDef)
+      )
+    "
+    @setPrimary="
+      (id) =>
+        $emit(
+          'update:widgetContents',
+          ops.makeSetPrimaryContentPayload(widgetContents, id)
+        )
+    "
+    @delete="
+      (id) =>
+        $emit(
+          'update:widgetContents',
+          ops.deleteWidgetContent(widgetContents, id)
+        )
+    "
+    @update:widgetContents="
+      (widgetContents) => {
+        $emit('update:widgetContents', widgetContents);
+      }
+    ">
+    <template #fieldContents="{ item }">
+      <div>
+        <label :for="`${item.id}-input`" class="sr-only">
+          {{ widgetDef.label }}
+        </label>
+        <textarea
+          :id="`${item.id}-input`"
+          :modelValue="item.fieldContents"
+          :placeholder="widgetDef.label"
+          class="bg-black/5 border-none rounded-md w-full text-sm"
+          @update:modelValue="
+            (value) =>
+              $emit(
+                'update:widgetContents',
+                ops.makeUpdateContentPayload(widgetContents, item.id, value)
+              )
+          " />
+      </div>
+    </template>
+  </EditWidgetLayout>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as Type from "@/types";
+import EditWidgetLayout from "./EditWidgetLayout.vue";
+import * as ops from "../editWidgetOps";
+
+defineProps<{
+  widgetDef: Type.RelatedAssetWidgetProps;
+  widgetContents: Type.WithId<Type.RelatedAssetWidgetContent>[];
+}>();
+
+defineEmits<{
+  (
+    e: "update:widgetContents",
+    widgetContents: Type.WithId<Type.RelatedAssetWidgetContent>[]
+  ): void;
+}>();
+</script>
 <style scoped></style>
+<style></style>
