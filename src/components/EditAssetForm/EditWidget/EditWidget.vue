61b10ea11065f385204763ac241dcd569324f540
James Johnson <johnsojr@umn.edu>
2025-04-28 10:51:32 -0700

    auto-open widget if focus is within

--- src/components/EditAssetForm/EditWidget/EditWidget.vue
+++ src/components/EditAssetForm/EditWidget/EditWidget.vue
@@ -1,15 +1,17 @@
 <template>
-  <component
-    :is="getWidgetComponentByType(widgetDef.type)"
-    :widgetDef="widgetDef"
-    :widgetContents="widgetContents"
-    :assetId="assetId"
-    :isOpen="isOpen"
-    @update:isOpen="$emit('update:isOpen', $event)"
-    @update:widgetContents="$emit('update:widgetContents', $event)" />
+  <div ref="editWidgetRef" class="edit-widget">
+    <component
+      :is="getWidgetComponentByType(widgetDef.type)"
+      :widgetDef="widgetDef"
+      :widgetContents="widgetContents"
+      :assetId="assetId"
+      :isOpen="isOpen"
+      @update:isOpen="$emit('update:isOpen', $event)"
+      @update:widgetContents="$emit('update:widgetContents', $event)" />
+  </div>
 </template>
 <script setup lang="ts">
-import { type Component } from "vue";
+import { type Component, ref, watch } from "vue";
 import type { WidgetProps, WidgetContent } from "@/types";
 import { WidgetType } from "@/types";
 import EditSelectWidget from "./EditSelectWidget.vue";
@@ -22,6 +24,7 @@ import EditUploadWidget from "./EditUploadWidget.vue";
 import EditTagWidget from "./EditTagWidget.vue";
 import EditRelatedAssetWidget from "./EditRelatedAssetWidget.vue";
 import EditTextWidget from "./EditTextWidget.vue";
+import { useFocusWithin } from "@vueuse/core";
 
 defineProps<{
   widgetDef: WidgetProps;
@@ -30,7 +33,7 @@ defineProps<{
   isOpen: boolean;
 }>();
 
-defineEmits<{
+const emit = defineEmits<{
   (e: "update:widgetContents", widgetContents: WidgetContent[]): void;
   (e: "update:isOpen", isOpen: boolean): void;
 }>();
@@ -52,5 +55,19 @@ const widgetMap: Record<WidgetType, Component> = {
 function getWidgetComponentByType(type: string) {
   return widgetMap[type] || null;
 }
+
+const editWidgetRef = ref<HTMLElement | null>(null);
+
+const { focused } = useFocusWithin(editWidgetRef);
+
+watch(
+  focused,
+  (isFocused) => {
+    if (isFocused) {
+      emit("update:isOpen", true);
+    }
+  },
+  { immediate: true }
+);
 </script>
 <style scoped></style>
