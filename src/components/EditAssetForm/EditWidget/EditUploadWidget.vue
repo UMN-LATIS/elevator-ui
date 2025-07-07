4103503b34257e9c046537a5bf935044c9b7003e
James Johnson <johnsojr@umn.edu>
2025-07-07 15:54:53 -0500

    layout upload widget content items

--- src/components/EditAssetForm/EditWidget/EditUploadWidget.vue
+++ src/components/EditAssetForm/EditWidget/EditUploadWidget.vue
@@ -31,40 +31,91 @@
       }
     ">
     <template #fieldContents="{ item }">
-      <ThumbnailImage
-        v-if="item.fileId"
-        :src="`${config.instance.base.url}/fileManager/tinyImageByFileId/${item.fileId}/true`"
-        :alt="item.fileDescription"
-        :fileType="item.fileType"
-        :showHoverIcon="false"
-        class="thumbnail-related-asset-widget__image max-w-full" />
       <FileUploader
-        v-else
+        v-if="!item.fileId"
         :widgetContent="item"
         :widgetDef="widgetDef"
         :collectionId="collectionId" />
-      <div>
-        <label :for="`${item.id}-input`" class="sr-only">
-          {{ widgetDef.label }}
-        </label>
-        <textarea
-          :id="`${item.id}-input`"
-          :value="JSON.stringify(item)"
-          :placeholder="widgetDef.label"
-          class="bg-black/5 border-none rounded-md w-full text-sm font-mono h-32"
-          @input="(event) => handleUpdateContent(item.id, (event.target as HTMLTextAreaElement).value)" />
+      <div v-else>
+        <div class="grid grid-cols-3 gap-4 mb-2">
+          <div class="w-full aspect-square rounded-md overflow-hidden relative">
+            <img
+              :src="`${config.instance.base.url}/fileManager/previewImageByFileId/${item.fileId}/true`"
+              :alt="`thumbnail for item: ${item.fileDescription}`"
+              class="w-full h-full object-cover" />
+          </div>
+
+          <div class="col-span-2 flex flex-col gap-1">
+            <label
+              :for="`${item.id}-description`"
+              class="block text-xs font-medium text-neutral-700 uppercase">
+              Description / Alt Text
+              <Tooltip
+                tip="Used by screen readers to describe the content of the file.">
+                <div
+                  class="inline-flex size-4 items-center justify-center bg-black/5 text-neutral-800 rounded-full text-xs">
+                  ?
+                </div>
+              </Tooltip>
+            </label>
+            <textarea
+              :id="`${item.id}-description`"
+              v-model="item.fileDescription"
+              placeholder="Enter a description for this file"
+              class="bg-black/5 border-none rounded-md w-full text-sm font-mono flex-1" />
+          </div>
+        </div>
+        <div v-if="isShowingDetails">
+          <Tuple label="File ID" class="text-sm text-neutral-600 mb-2">
+            {{ item.fileId }}
+          </Tuple>
+          <Tuple label="File Type" class="text-sm text-neutral-600 mb-2">
+            {{ item.fileType }}
+          </Tuple>
+          <Tuple
+            v-for="(value, key) in item.sidecars"
+            :key="key"
+            :label="`Sidecar: ${key}`"
+            class="text-sm text-neutral-600 mb-2">
+            {{ value || "-" }}
+          </Tuple>
+          <Tuple label="Location" class="text-sm text-neutral-600 mb-2">
+            <pre>{{ item.loc || "-" }}</pre>
+          </Tuple>
+          <Tuple label="Search Data" class="text-sm text-neutral-600 mb-2">
+            <pre>{{ item.searchData || "-" }}</pre>
+          </Tuple>
+          <label :for="`${item.id}-input`">JSON</label>
+          <textarea
+            :id="`${item.id}-input`"
+            :value="JSON.stringify(item)"
+            :placeholder="widgetDef.label"
+            class="bg-black/5 border-none rounded-md w-full text-sm font-mono h-32"
+            @input="(event) => handleUpdateContent(item.id, (event.target as HTMLTextAreaElement).value)" />
+        </div>
+        <button
+          class="flex items-center justify-center gap-2 text-sm text-neutral-600 hover:text-neutral-900 w-full p-2"
+          @click="isShowingDetails = !isShowingDetails">
+          <ChevronDownIcon
+            class="w-4 h-4 transition-transform"
+            :class="{ 'rotate-180': isShowingDetails }" />
+          <span class="sr-only">Toggle Details</span>
+        </button>
       </div>
     </template>
   </EditWidgetLayout>
 </template>
 <script setup lang="ts">
+import { ref } from "vue";
 import * as Type from "@/types";
 import EditWidgetLayout from "./EditWidgetLayout.vue";
 import * as ops from "../editWidgetOps";
 import invariant from "tiny-invariant";
 import FileUploader from "./FileUploader.vue";
 import config from "@/config";
-import ThumbnailImage from "@/components/ThumbnailImage/ThumbnailImage.vue";
+import Tooltip from "@/components/Tooltip/Tooltip.vue";
+import { ChevronDownIcon } from "lucide-vue-next";
+import Tuple from "@/components/Tuple/Tuple.vue";
 
 const props = defineProps<{
   collectionId: number;
@@ -80,6 +131,8 @@ const emit = defineEmits<{
   );
   (e: "update:isOpen", isOpen: boolean): void;
 }>();
+
+const isShowingDetails = ref(false);
 
 function handleUpdateContent(id: string, value: string) {
   const parsedValue = JSON.parse(
@@ -103,6 +156,14 @@ function handleUpdateContent(id: string, value: string) {
 
   emit("update:widgetContents", updatedContents);
 }
+
+// listen for Command + Control + H to show/hide details
+document.addEventListener("keydown", (event) => {
+  if (event.key === "h" && event.ctrlKey && event.metaKey) {
+    event.preventDefault();
+    isShowingDetails.value = !isShowingDetails.value;
+  }
+});
 </script>
 <style scoped></style>
 <style></style>
