7ac84d0f72b0c0b494b1b502bcfb99431587c905
James Johnson <johnsojr@umn.edu>
2025-07-15 15:29:08 -0500

    ply handler sidecar

--- src/pages/CreateOrEditAssetPage/EditWidget/EditUploadWidget/Sidecars/PlyHandlerSidecar.vue
+++ src/pages/CreateOrEditAssetPage/EditWidget/EditUploadWidget/Sidecars/PlyHandlerSidecar.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="">Ply Handler Sidecar</div>
+  <div class="ply-handler-sidecar">
+    <UploadableTextArea
+      :modelValue="sidecars['3dpoints'] ?? ''"
+      label="3D Points"
+      placeholder="No 3D points"
+      @update:modelValue="
+        (value) =>
+          $emit('update:sidecars', {
+            ...sidecars,
+            ['3dpoints']: value,
+          })
+      " />
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as Type from "@/types";
+import { FileMetaData } from "@/types/FileMetaDataTypes";
+import UploadableTextArea from "./UploadableTextArea.vue";
+
+defineProps<{
+  sidecars: Type.WithId<Type.UploadWidgetContent["sidecars"]>;
+  widgetDef: Type.UploadWidgetDef;
+  fileMetaData: FileMetaData | null;
+}>();
+
+defineEmits<{
+  (
+    e: "update:sidecars",
+    sidecars: Type.WithId<Type.UploadWidgetContent["sidecars"]>
+  ): void;
+}>();
+</script>
 <style scoped></style>
