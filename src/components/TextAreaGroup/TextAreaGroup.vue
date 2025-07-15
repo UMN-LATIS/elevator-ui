b7464a6679593e60433fb20fe32d50d8ccc4c443
James Johnson <johnsojr@umn.edu>
2025-07-15 13:12:20 -0500

    update TextAreaGroup component

---
+++ src/components/TextAreaGroup/TextAreaGroup.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="text-area">
+    <div class="flex justify-between items-baseline">
+      <slot :id="`textarea-${id}`" name="label">
+        <label
+          :for="`textarea-${id}`"
+          :class="
+            cn(
+              'block text-xs font-medium text-neutral-700 uppercase mb-1',
+              labelClass
+            )
+          ">
+          {{ label }}
+        </label>
+      </slot>
+      <div>
+        <slot name="corner" />
+      </div>
+    </div>
+    <div class="mt-1 flex rounded-md shadow-sm relative">
+      <textarea
+        :id="`textarea-${id}`"
+        :name="`textarea-${id}`"
+        :class="
+          cn(
+            'text-area-input block w-full rounded border-neutral-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2 font-mono text-neutral-500 focus:text-neutral-900 h-24',
+            inputClass
+          )
+        "
+        :placeholder="placeholder"
+        :value="modelValue"
+        :readonly="readonly"
+        @input="(event) => $emit('update:modelValue', (event.target as HTMLTextAreaElement).value)" />
+    </div>
+    <slot name="append" />
+  </div>
+</template>
+<script setup lang="ts">
+import { StyleValue, useId } from "vue";
+import { cn } from "@/lib/utils";
+
+withDefaults(
+  defineProps<{
+    label: string;
+    modelValue: string;
+    placeholder?: string;
+    inputClass?: StyleValue;
+    labelClass?: StyleValue;
+    readonly?: boolean;
+  }>(),
+  {
+    placeholder: "",
+    inputClass: "",
+    labelClass: "",
+    readonly: false,
+  }
+);
+
+defineEmits<{
+  (eventName: "update:modelValue", value: string): void;
+}>();
+
+const id = useId();
+</script>
+<style scoped></style>
