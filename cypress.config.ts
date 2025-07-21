2bf81ff8c62d532e1dfef3f2d50af7d6cda90d50
James Johnson <johnsojr@umn.edu>
2025-07-21 15:49:08 -0500

    ignore unused vars with _

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
