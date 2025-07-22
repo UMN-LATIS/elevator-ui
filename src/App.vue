817540dca6b2a260eb23b9de32da9881dd18daf8
James Johnson <johnsojr@umn.edu>
2025-07-22 09:18:52 -0500

    add visual indicator when using mock backend

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <div
+      v-if="config.isUsingMockServer"
+      :class="{
+        'border-[0.5rem] border-pink-500 pointer-events-none fixed inset-0 z-[9999]':
+          config.isUsingMockServer,
+      }">
+      <p class="text-center text-pink-500 font-bold uppercase text-xs">
+        using mock backend
+      </p>
+    </div>
     <Teleport to="body">
       <ErrorModal />
       <ToastRoot />
@@ -15,6 +25,7 @@ import { useTheming } from "./helpers/useTheming";
 import { useElevatorSessionStorage } from "./helpers/useElevatorSessionStorage";
 import ErrorModal from "@/components/ErrorModal/ErrorModal.vue";
 import ToastRoot from "@/components/ToastRoot/ToastRoot.vue";
+import config from "@/config";
 
 // load instance store before mounting app
 // this prevents a race conditiion where the search store
