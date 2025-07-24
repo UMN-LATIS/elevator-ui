b5f5f5afae70b0d9b7212a0f0d78f7603987074d
James Johnson <johnsojr@umn.edu>
2025-07-24 14:31:15 -0500

    Revert "Update src/components/XButton/XButton.vue"
    This reverts commit 359ace19d92f2fa91fdd70399e85d9766942c8c0.

--- src/components/XButton/XButton.vue
+++ src/components/XButton/XButton.vue
@@ -3,11 +3,9 @@
     class="x-button inline-flex bg-transparent-black-50 text-neutral-700 w-8 h-8 rounded-full leading-none items-center justify-center transition ease-in-out hover:bg-transparent-black-900 hover:text-white"
     @click="$emit('click')">
     <XIcon />
-    <slot v-slot="slotProps">
-      <span v-if="!slotProps?.default" class="sr-only">Close</span>
-      <template v-else>
-        <slot />
-      </template>
+    <slot>
+      <span class="sr-only">Close</span>
+    </slot>
   </button>
 </template>
 <script setup lang="ts">
