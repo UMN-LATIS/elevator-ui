a87670995892a6fead3fc8b91a4ba1252c5ea184
James Johnson <johnsojr@umn.edu>
2025-07-31 15:38:46 -0500

    handle numeric selectgroup ids

--- src/components/SelectGroup/SelectGroup.vue
+++ src/components/SelectGroup/SelectGroup.vue
@@ -21,7 +21,7 @@
       readonly
       required
       @change="
-        $emit('update:modelValue', ($event.target as HTMLSelectElement).value)
+        handleUpdateSelection(($event.target as HTMLSelectElement).value)
       ">
       <option value="" disabled selected>{{ placeholder }}</option>
       <option v-for="opt in options" :key="opt.id" :value="opt.id">
@@ -36,7 +36,7 @@ import { CSSClass } from "@/types";
 
 withDefaults(
   defineProps<{
-    modelValue: string | number;
+    modelValue: string | number | null;
     label: string;
     options: Array<{
       id: string | number;
@@ -59,8 +59,24 @@ withDefaults(
   }
 );
 
-defineEmits<{
-  (e: "update:modelValue", value: string | number): void;
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string | number | null): void;
 }>();
+
+function handleUpdateSelection(value: string | number) {
+  // if the value is empty, set it to null
+  if (value === "") {
+    return emit("update:modelValue", null);
+  }
+
+  // if the value is a number, convert it to a number
+  const maybeNumber = Number(value);
+  if (Number.isInteger(maybeNumber)) {
+    return emit("update:modelValue", maybeNumber);
+  }
+
+  // otherwise, return the value as a string
+  return emit("update:modelValue", value);
+}
 </script>
 <style scoped></style>
