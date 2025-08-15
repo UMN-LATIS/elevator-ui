2bbca459c797d50c0cf830b5f79c9cfe4f793739
James Johnson <johnsojr@umn.edu>
2025-08-15 13:26:15 -0500

    start simplified CascadeSelect

---
+++ src/components/CascadeSelect/SimpleCascadeSelect.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class=""></div>
+</template>
+<script setup lang="ts">
+import {
+  type NestedOptionsObj,
+  useCascadeSelect,
+} from "@/composables/useCascadeSelect";
+import { MultiSelectWidgetContent } from "@/types";
+
+const props = defineProps<{
+  options: NestedOptionsObj;
+  modelValue: MultiSelectWidgetContent["fieldContents"];
+  selectClass?: Record<string, boolean> | string[] | string;
+  labelClass?: Record<string, boolean> | string[] | string;
+  initialSelectedValues?: string[];
+}>();
+
+defineEmits<{
+  (e: "update:modelValue", updated: MultiSelectWidgetContent["fieldContents"]);
+}>();
+</script>
+<style scoped></style>
--- src/composables/useCascadeSelect.test.ts
+++ src/composables/useCascadeSelect.test.ts
@@ -1,10 +1,5 @@
 import { describe, it, expect } from "vitest";
-import {
-  type FlatOption,
-  type Level,
-  type NestedOptionsObj,
-  useCascadeSelect,
-} from "./useCascadeSelect";
+import { type NestedOptionsObj, useCascadeSelect } from "./useCascadeSelect";
 
 const nestedOptions: NestedOptionsObj = {
   country: {
