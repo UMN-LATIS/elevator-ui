fd2f229555f8097864153c8253e81157d704af0a
James Johnson <johnsojr@umn.edu>
2025-09-18 18:45:09 -0500

    add file source name and download to upload widget

--- src/pages/CreateOrEditAssetPage/EditWidget/EditUploadWidget/EditUploadWidgetItemSidecars.vue
+++ src/pages/CreateOrEditAssetPage/EditWidget/EditUploadWidget/EditUploadWidgetItemSidecars.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="upload-widget-item-sidecars">
-    <!-- <h3 class="text-sm font-semibold text-neutral-800 mb-2">Sidecars</h3>
-    <p class="text-sm text-neutral-600 mb-2">
-      Sidecars are additional metadata or files associated with the main file.
-    </p> -->
     <component
       :is="sidecarComponent"
       v-if="sidecarComponent"
       :sidecars="item.sidecars"
       :widgetDef="widgetDef"
-      :fileMetaData="fileMetadata"
+      :fileMetaData="fileMetaData"
       @update:sidecars="$emit('update:item', { ...item, sidecars: $event })" />
   </div>
 </template>
 <script setup lang="ts">
 import * as Type from "@/types";
-import { useFileMetadataQuery } from "@/queries/useFileMetadataQuery";
 import { PartialRecord } from "ramda";
 import { type Component, computed } from "vue";
 import ImageHandlerSidecar from "./Sidecars/ImageHandlerSidecar.vue";
@@ -24,29 +19,21 @@ import MovieHandlerSidecar from "./Sidecars/MovieHandlerSidecar.vue";
 import ObjHandlerSidecar from "./Sidecars/ObjHandlerSidecar.vue";
 import PlyHandlerSidecar from "./Sidecars/PlyHandlerSidecar.vue";
 import ZipObjHandlerSidecar from "./Sidecars/ZipObjHandlerSidecar.vue";
+import { FileMetaData } from "@/types/FileMetaDataTypes";
 
 const props = defineProps<{
   item: Type.WithId<Type.UploadWidgetContent>;
   widgetDef: Type.UploadWidgetDef;
+  fileMetaData?: FileMetaData;
 }>();
 
 defineEmits<{
   (e: "update:item", item: Type.WithId<Type.UploadWidgetContent>): void;
 }>();
 
-const { data: fileMetadata } = useFileMetadataQuery(() => props.item.fileId);
-
 const fileHandlerName = computed(
-  () => fileMetadata?.value?.handlerType?.toLowerCase() ?? null
+  () => props.fileMetaData?.handlerType?.toLowerCase() ?? null
 );
-
-type IsEnabledFn = ({
-  item,
-  widgetDef,
-}: {
-  item: Type.WithId<Type.UploadWidgetContent>;
-  widgetDef: Type.UploadWidgetDef;
-}) => boolean;
 
 type FileHandlerType =
   | "audiohandler"
@@ -77,38 +64,5 @@ const sidecarComponent = computed(() => {
   }
   return component;
 });
-
-// const sidecarRegistry: PartialRecord<FileHandlerType, Sidecar[]> = {
-//   imagehandler: [
-//     {
-//       name: "ppm",
-//       label: "Pixels per Millimeter",
-//       component: "input",
-//       placeholder: "Optional value",
-//       isEnabled: () => true,
-//     },
-//     {
-//       name: "iframe",
-//       label: "iframe url",
-//       component: "input",
-//       placeholder: "HTTPS highly recommended",
-//       isEnabled: ({ widgetDef }) => widgetDef.fieldData.enableIframe ?? false,
-//     },
-//     {
-//       name: "dendro",
-//       label: "Dendro Data",
-//       component: "input",
-//       placeholder: "",
-//       isEnabled: ({ widgetDef }) => widgetDef.fieldData.enableDendro ?? false,
-//     },
-//     {
-//       name: "svs",
-//       label: "SVS Data",
-//       component: "input",
-//       placeholder: "",
-//       isEnabled: ({ item, widgetDef }) => !!item.sidecars?.svs,
-//     },
-//   ],
-// };
 </script>
 <style scoped></style>
