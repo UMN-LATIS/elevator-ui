e729bc3a4accc409534e7c74560e8bd72654e06a
James Johnson <johnsojr@umn.edu>
2025-09-19 17:22:19 -0500

    fix collection item borders and indenting

--- src/pages/AllCollectionsPage/CollectionItem.vue
+++ src/pages/AllCollectionsPage/CollectionItem.vue
@@ -1,20 +1,20 @@
 <template>
-  <div
-    class="all-collections-page__collection-item p-4 rounded border transition-colors duration-150">
-    <div class="flex items-center gap-1">
+  <div class="collection-item rounded-md">
+    <div class="collection-item__parent flex items-center rounded-md">
       <button
         v-if="collection.children && collection.children.length > 0"
-        class="p-2 -ml-2 rounded-full inline-flex items-center justify-center hover:bg-white transition-colors duration-150"
+        class="collection-item__parent-expand-button p-3 inline-flex items-center rounded-md justify-center duration-150"
         @click="showMore = !showMore">
-        <ChevronDownIcon
-          class="transform w-4 h-4"
-          :class="{
-            '-rotate-90': !showMore,
-          }" />
+        <ChevronDown v-if="showMore" class="size-4" />
+        <ChevronRight v-else class="size-4" />
       </button>
       <Link
-        class="flex gap-2 flex-1 items-center !no-underline h-full"
-        :to="`/collections/browseCollection/${collection.id}`">
+        class="collection-item__parent-link flex gap-2 flex-1 items-center !no-underline h-full p-2 rounded-md"
+        :to="`/collections/browseCollection/${collection.id}`"
+        :class="{
+          // add padding if root collections have no children
+          'pl-4': nestingLevel === 0 && !collection.children?.length,
+        }">
         <LazyLoadImage
           v-if="imgSrc"
           :src="imgSrc"
@@ -23,13 +23,13 @@
         <h2 :class="{ 'font-bold': showMore }">{{ collection.title }}</h2>
       </Link>
     </div>
-    <div v-if="viewableChildren.length">
+    <div v-if="viewableChildren.length" class="collection-item__children">
       <CollectionItem
         v-for="child in viewableChildren"
         v-show="showMore"
         :key="child.id"
         :collection="child"
-        class="!p-0 m-2 ml-8" />
+        class="ml-8" />
     </div>
   </div>
 </template>
@@ -37,13 +37,19 @@
 import { computed, ref } from "vue";
 import { getThumbURL } from "@/helpers/displayUtils";
 import { AssetCollection } from "@/types";
-import { ChevronDownIcon } from "@/icons";
 import Link from "@/components/Link/Link.vue";
 import LazyLoadImage from "@/components/LazyLoadImage/LazyLoadImage.vue";
+import { ChevronDown, ChevronRight } from "lucide-vue-next";
 
-const props = defineProps<{
-  collection: AssetCollection;
-}>();
+const props = withDefaults(
+  defineProps<{
+    collection: AssetCollection;
+    nestingLevel?: number;
+  }>(),
+  {
+    nestingLevel: 0,
+  }
+);
 
 const imgSrc = computed((): string | null => {
   const imgId = props.collection.previewImageId;
@@ -70,7 +76,8 @@ const showMore = ref(false);
   }
 }
 
-.collection-item:hover {
+.collection-item__parent-link:hover,
+.collection-item__parent-expand-button:hover {
   --hover-text-color: var(--color-blue-600);
   --hover-bg-color: var(--color-blue-50);
   background: var(--hover-bg-color);
