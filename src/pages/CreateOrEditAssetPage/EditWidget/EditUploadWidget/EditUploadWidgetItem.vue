d7ba39875f185c1f1927dbfd3483ea02803489a5
James Johnson <johnsojr@umn.edu>
2025-09-19 12:48:22 -0500

    update upload widget in asset editor with download link (#381)

--- src/pages/CreateOrEditAssetPage/EditWidget/EditUploadWidget/EditUploadWidgetItem.vue
+++ src/pages/CreateOrEditAssetPage/EditWidget/EditUploadWidget/EditUploadWidgetItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="edit-upload-widget-item">
-    <div class="grid grid-cols-3 gap-4 mb-2">
+    <div class="grid grid-cols-3 gap-x-4 gap-y-3 mb-2">
       <div class="w-full aspect-square rounded-md overflow-hidden relative">
         <img
           v-if="item.fileId && previewImageUrl"
@@ -20,7 +20,7 @@
       <div class="col-span-2 flex flex-col gap-1">
         <label
           :for="`${item.id}-description`"
-          class="block text-xs font-medium text-neutral-700 uppercase">
+          class="block text-xs font-medium uppercase">
           Description / Alt Text
           <Tooltip
             tip="Used by screen readers to describe the content of the file.">
@@ -36,32 +36,47 @@
           class="bg-black/5 border-none rounded-md w-full text-sm font-mono flex-1 placeholder:text-neutral-400"
           @input="handleDescriptionUpdate" />
       </div>
+      <div class="col-span-3">
+        <Tuple
+          label="Source File"
+          class="flex items-center justify-between flex-wrap text-sm gap-2 -mt-2">
+          {{ fileMetaData?.sourcefile }}
+          <Button
+            v-if="fileMetaData?.sourcefile"
+            :href="`${config.instance.base.url}/fileManager/getOriginal/${item.fileId}`"
+            variant="tertiary"
+            class="flex items-center gap-1 !no-underline text-xs uppercase font-medium"
+            download>
+            <DownloadIcon class="!size-4" />
+            Download
+          </Button>
+        </Tuple>
+      </div>
       <EditUploadWidgetItemSidecars
         class="col-span-3"
         :item="item"
         :widgetDef="widgetDef"
+        :fileMetaData="fileMetaData"
         @update:item="$emit('update:item', $event)" />
 
       <section
         v-if="isShowingDetails"
-        class="col-span-3 p-4 border border-black/15 rounded-md">
-        <h2 class="text-sm font-semibold text-neutral-800 mb-2">
-          File Details
-        </h2>
-        <Tuple label="File ID" class="text-sm text-neutral-600 mb-2">
+        class="col-span-3 py-3 border-y border-black/15 text-sm flex flex-col gap-2">
+        <h2 class="sr-only">File Details</h2>
+        <Tuple label="File ID">
           {{ item.fileId }}
         </Tuple>
-        <Tuple label="File Type" class="text-sm text-neutral-600 mb-2">
+        <Tuple label="File Type">
           {{ item.fileType }}
         </Tuple>
 
-        <Tuple label="Location" class="text-sm text-neutral-600 mb-2">
+        <Tuple label="Location">
           <pre>{{ item.loc || "-" }}</pre>
         </Tuple>
-        <Tuple label="Search Data" class="text-sm text-neutral-600 mb-2">
+        <Tuple label="Search Data">
           <pre>{{ item.searchData || "-" }}</pre>
         </Tuple>
-        <div class="flex flex-col gap-4 mt-2">
+        <div class="flex justify-between gap-4 flex-wrap">
           <label
             class="flex items-center gap-2 text-sm text-neutral-600 hover:text-neutral-900 transition-colors">
             <input
@@ -73,9 +88,11 @@
           </label>
           <Button
             :href="`${config.instance.base.url}/assetManager/processingLogsForAsset/${item.fileId}`"
+            variant="tertiary"
             target="_blank"
             class="text-sm !px-3 !py-2">
-            View Processing Logs
+            <FileCogIcon class="!size-4" />
+            View Logs
           </Button>
         </div>
       </section>
@@ -96,12 +113,17 @@
 import * as Type from "@/types";
 import config from "@/config";
 import Tooltip from "@/components/Tooltip/Tooltip.vue";
-import { ChevronDownIcon, FileIcon } from "lucide-vue-next";
+import {
+  ChevronDownIcon,
+  FileCogIcon,
+  FileIcon,
+  DownloadIcon,
+} from "lucide-vue-next";
 import Tuple from "@/components/Tuple/Tuple.vue";
 import Button from "@/components/Button/Button.vue";
-import { SpinnerIcon } from "@/icons";
 import EditUploadWidgetItemSidecars from "./EditUploadWidgetItemSidecars.vue";
 import { usePreviewImage } from "@/helpers/usePreviewImage";
+import { useFileMetadataQuery } from "@/queries/useFileMetadataQuery";
 
 const props = defineProps<{
   item: Type.WithId<Type.UploadWidgetContent>;
@@ -116,6 +138,8 @@ const emit = defineEmits<{
 
 // Use the new preview image composable
 const { previewImageUrl } = usePreviewImage(() => props.item.fileId);
+
+const { data: fileMetaData } = useFileMetadataQuery(() => props.item.fileId);
 
 function handleDescriptionUpdate(event: Event) {
   const target = event.target as HTMLTextAreaElement;
