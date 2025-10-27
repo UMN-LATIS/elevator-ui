3ad9fb92c76beb2269aa7da99cc6e6257af0f5c7
James Johnson <johnsojr@umn.edu>
2025-10-27 13:53:53 -0500

    cleanup

--- src/components/CustomAppHeader/CustomAppHeader.vue
+++ src/components/CustomAppHeader/CustomAppHeader.vue
@@ -1,36 +1,17 @@
 <template>
   <div class="custom-app-header bg-transparent-black-100">
     <SanitizedHTML
-      :html="htmlWithoutScripts"
-      :addTags="['style', 'link']"
+      :html="instanceStore?.customHeader ?? ''"
+      :addTags="['style', 'link', 'script']"
+      :permitScripts="true"
       class="w-full" />
   </div>
 </template>
 <script setup lang="ts">
-import { computed, nextTick, onMounted, onUnmounted } from "vue";
 import { useInstanceStore } from "@/stores/instanceStore";
-import { useScripts } from "@/composables/useScripts";
+// import { useScripts } from "@/composables/useScripts";
 import SanitizedHTML from "../SanitizedHTML/SanitizedHTML.vue";
 
 const instanceStore = useInstanceStore();
-
-const rawHtml = computed(() => instanceStore?.customHeader ?? "");
-
-const scripts = useScripts();
-
-const htmlWithoutScripts = computed(() =>
-  scripts.stripScriptsFromHtml(rawHtml.value)
-);
-
-onMounted(async () => {
-  // run any scripts found in the custom header HTML
-  await nextTick();
-  scripts.run(rawHtml.value);
-});
-
-onUnmounted(() => {
-  // clean up any scripts added by the custom header HTML
-  scripts.cleanup();
-});
 </script>
 <style scoped></style>
--- src/components/SanitizedHTML/SanitizedHTML.vue
+++ src/components/SanitizedHTML/SanitizedHTML.vue
@@ -4,23 +4,39 @@
 </template>
 <script setup lang="ts">
 import DOMPurify from "dompurify";
-import { computed } from "vue";
+import { uniq } from "ramda";
+import {
+  computed,
+  onMounted,
+  useTemplateRef,
+  nextTick,
+  onBeforeUnmount,
+} from "vue";
 
 const props = withDefaults(
   defineProps<{
     html: string;
     removeInlineStyles?: boolean;
     addTags?: string[];
+    permitScripts?: boolean;
   }>(),
   {
     removeInlineStyles: false,
     addTags: () => [],
+    permitScripts: false,
   }
 );
 
+const emit = defineEmits<{
+  (e: "contentLoaded"): void;
+  (e: "imagesLoaded"): void;
+}>();
+
+const containerRef = useTemplateRef<HTMLDivElement | null>("containerRef");
+
 const sanitizeConfig = {
   FORBID_ATTR: props.removeInlineStyles ? ["style"] : [],
-  ADD_TAGS: ["iframe", ...props.addTags],
+  ADD_TAGS: uniq(["iframe", ...props.addTags]),
   // needed for <style> tags
   // https://github.com/cure53/DOMPurify/issues/257#issuecomment-346384997
   FORCE_BODY: props.addTags?.includes("style"),
@@ -28,5 +44,88 @@ const sanitizeConfig = {
 
 const sanitizedHtml = computed(() => {
   return DOMPurify.sanitize(props.html, sanitizeConfig);
+});
+
+const runningScripts: HTMLScriptElement[] = [];
+
+function getScriptsFromHtml(html: string): HTMLScriptElement[] {
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = html;
+
+  const scriptNodeList = tempDiv.querySelectorAll("script");
+  return Array.from(scriptNodeList);
+}
+
+function cleanupScripts() {
+  runningScripts.forEach((script) => {
+    if (script.parentNode) {
+      script.parentNode.removeChild(script);
+    }
+  });
+  runningScripts.splice(0, runningScripts.length);
+}
+
+function runScripts(html: string) {
+  const scripts = getScriptsFromHtml(html);
+  scripts.forEach(async (originalScript) => {
+    const script = document.createElement("script");
+
+    // Copy attributes
+    Array.from(originalScript.attributes).forEach((attr) => {
+      script.setAttribute(attr.name, attr.value);
+    });
+
+    // Copy content if it's an inline script
+    if (originalScript.textContent) {
+      script.textContent = originalScript.textContent;
+    }
+
+    // Append to body to execute
+    document.body.appendChild(script);
+    runningScripts.push(script);
+  });
+}
+
+onMounted(async () => {
+  // wait for dom updates
+  await nextTick();
+  emit("contentLoaded");
+
+  if (!props.permitScripts) return;
+
+  const container = containerRef.value;
+  if (!container) {
+    throw new Error("Container ref is null");
+  }
+
+  // cleanup any previous running scripts
+  // adding this in case we change to a watcher
+  cleanupScripts();
+
+  // get images
+  const images = Array.from(
+    containerRef.value.querySelectorAll<HTMLImageElement>("img")
+  );
+
+  await Promise.allSettled(
+    images.map((img) => {
+      if (img.complete) return Promise.resolve();
+      return new Promise<void>((resolve, reject) => {
+        img.addEventListener("load", () => resolve());
+        img.addEventListener("error", () => {
+          console.warn("Image failed to load:", img.src);
+          reject();
+        });
+      });
+    })
+  );
+  runScripts(props.html);
+
+  // emit after scripts run
+  emit("imagesLoaded");
+});
+
+onBeforeUnmount(() => {
+  cleanupScripts();
 });
 </script>
