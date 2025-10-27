23935ae40495d09a695e5305065ce6577e0aca74
James Johnson <johnsojr@umn.edu>
2025-10-27 11:10:32 -0500

    run header and footer scripts

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -2,14 +2,35 @@
   <footer
     v-if="instanceStore.customFooter"
     class="app-footer bg-transparent-black-100">
-    <SanitizedHTML :html="instanceStore.customFooter" :addTags="['style', 'link', 'script']" />
+    <SanitizedHTML :html="htmlWithoutScripts" :addTags="['style', 'link']" />
   </footer>
 </template>
 <script setup lang="ts">
+import { onMounted, computed, nextTick, onUnmounted } from "vue";
 import { useInstanceStore } from "@/stores/instanceStore";
 import SanitizedHTML from "../SanitizedHTML/SanitizedHTML.vue";
+import { useScripts } from "@/composables/useScripts";
 
 const instanceStore = useInstanceStore();
+
+const rawHtml = computed(() => instanceStore?.customFooter ?? "");
+
+const scripts = useScripts();
+
+const htmlWithoutScripts = computed(() =>
+  scripts.stripScriptsFromHtml(rawHtml.value)
+);
+
+onMounted(async () => {
+  // run any scripts found in the custom header HTML
+  await nextTick();
+  scripts.run(rawHtml.value);
+});
+
+onUnmounted(() => {
+  // clean up any scripts added by the custom header HTML
+  scripts.cleanup();
+});
 </script>
 <style scoped>
 .app-footer {
--- src/components/CustomAppHeader/CustomAppHeader.vue
+++ src/components/CustomAppHeader/CustomAppHeader.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="custom-app-header bg-transparent-black-100">
     <SanitizedHTML
-      :html="instanceStore?.customHeader ?? ''"
-      :addTags="['style', 'link', 'script']" />
+      :html="htmlWithoutScripts"
+      :addTags="['style', 'link']"
+      class="w-full" />
   </div>
 </template>
 <script setup lang="ts">
+import { computed, nextTick, onMounted, onUnmounted } from "vue";
 import { useInstanceStore } from "@/stores/instanceStore";
+import { useScripts } from "@/composables/useScripts";
 import SanitizedHTML from "../SanitizedHTML/SanitizedHTML.vue";
 
 const instanceStore = useInstanceStore();
+
+const rawHtml = computed(() => instanceStore?.customHeader ?? "");
+
+const scripts = useScripts();
+
+const htmlWithoutScripts = computed(() =>
+  scripts.stripScriptsFromHtml(rawHtml.value)
+);
+
+onMounted(async () => {
+  // run any scripts found in the custom header HTML
+  await nextTick();
+  scripts.run(rawHtml.value);
+});
+
+onUnmounted(() => {
+  // clean up any scripts added by the custom header HTML
+  scripts.cleanup();
+});
 </script>
 <style scoped></style>
