b7b98fb34f2eafaac8ba8604f9b328b23e4e8eae
James Johnson <johnsojr@umn.edu>
2026-01-21 14:50:21 -0600

    add error handler of last resort

--- src/main.ts
+++ src/main.ts
@@ -18,4 +18,27 @@ const app = createApp(App);
 const pinia = createPinia();
 pinia.use(resetStorePlugin);
 
-app.use(router).use(pinia).use(VueQueryPlugin).mount("#app");
+app.use(router).use(pinia).use(VueQueryPlugin);
+
+// error handler of last resort. It bails after 10 errors to avoid infinite loops.
+let errorCount = 0;
+let timeoutId: ReturnType<typeof setTimeout> | null = null;
+app.config.errorHandler = (err, instance, info) => {
+  const MAX_ERRORS = 10;
+  errorCount++;
+  console.error(`Error #${errorCount}:`, info, err);
+
+  if (errorCount > MAX_ERRORS) {
+    throw new Error("Too many errors - stopping to prevent loop");
+  }
+
+  // Reset error count after 10 seconds without errors
+  if (!timeoutId) {
+    timeoutId = setTimeout(() => {
+      errorCount = 0;
+      timeoutId = null;
+    }, 10000);
+  }
+};
+
+app.mount("#app");
