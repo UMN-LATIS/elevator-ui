12dc10f250b7db573aa1289b8a6272fcccbbe898
James Johnson <johnsojr@umn.edu>
2026-01-28 13:01:46 -0600

    revert this suggestion too

--- src/pages/CreateOrEditAssetPage/EditWidget/EditTagWidget.vue
+++ src/pages/CreateOrEditAssetPage/EditWidget/EditTagWidget.vue
@@ -39,7 +39,7 @@
           :templateId="templateId"
           inputClass="!py-0 flex-1 min-w-24"
           :blurOnSelect="false"
-          @blur="(value) => handleTagUpdate(item.id, value)"
+          @blur="handleTagUpdate(item.id, tagInput)"
           @select="(selection) => handleTagUpdate(item.id, selection)"
           @keydown="(event) => handleKeydown(item.id, event)" />
         <TagsInputInput v-else :placeholder="`${widgetDef.label}...`" />
