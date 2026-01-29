80866248ab2f67d8b6d48e12e4a6fe06f5ef14e3
James Johnson <johnsojr@umn.edu>
2026-01-29 10:47:03 -0600

    wip

--- src/pages/InstanceSettingsPage/InstanceSettingsPage.vue
+++ src/pages/InstanceSettingsPage/InstanceSettingsPage.vue
@@ -1,6 +1,15 @@
 <template>
   <DefaultLayout>
-    <div class="p-8 px-4">Instance Settings page</div>
+    <div class="p-8 px-4">
+      <header>
+        <p class="text-2xl md:text-4xl font-bold text-neutral-400">Admin</p>
+        <h1 class="text-2xl md:text-4xl font-bold">Instance Settings</h1>
+      </header>
+
+      <section>
+        <h2>General</h2>
+      </section>
+    </div>
   </DefaultLayout>
 </template>
 <script setup lang="ts">
