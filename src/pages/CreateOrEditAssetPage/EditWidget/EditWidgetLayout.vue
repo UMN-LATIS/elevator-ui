15af9ff4f412862c008e1f705518d589e9f4e2f1
James Johnson <johnsojr@umn.edu>
2026-02-08 16:55:10 -0600

    fixes

--- src/pages/CreateOrEditAssetPage/EditWidget/EditWidgetLayout.vue
+++ src/pages/CreateOrEditAssetPage/EditWidget/EditWidgetLayout.vue
@@ -12,7 +12,7 @@
         {{ widgetDef.label }}
       </h2>
       <div
-        class="edit-widget-layout__accordion-button-wrapper flex gap-2 justify-between lg:w-48 xl:w-xs items-center bg-white/10 backdrop-blur-md"
+        class="edit-widget-layout__accordion-button-wrapper flex gap-2 justify-between lg:w-48 xl:w-xs items-center backdrop-blur-md"
         :class="{
           'sticky top-[4rem] z-10': isOpen,
         }">
