898d4c86ee213e6c3ad55ee02ae8486aad9db177
James Johnson <johnsojr@umn.edu>
2026-02-11 19:56:01 -0600

    fix theming

--- src/helpers/useTheming.ts
+++ src/helpers/useTheming.ts
@@ -2,12 +2,7 @@ import { watch, computed } from "vue";
 import { useStorage } from "@vueuse/core";
 import { useInstanceQuery } from "@/queries/useInstanceQuery";
 
-function getBaseUrl() {
-  return window.location.pathname.split("/")[1]; // get the first part of the url to use as the key for local storage
-}
-
 export function useTheming() {
-  // const instanceStore = useInstanceStore();
   const { data: instanceData } = useInstanceQuery();
   const availableThemes = computed(
     () => instanceData.value?.theming?.availableThemes || ["light", "dark"]
@@ -19,17 +14,23 @@ export function useTheming() {
     () => instanceData.value?.theming?.enabled ?? true
   );
 
-  const baseUrl = getBaseUrl();
-
-  const activeTheme = useStorage(`theme-${baseUrl}`, defaultTheme.value);
+  const activeTheme = useStorage<string | null>(
+    `theme-${window.location.hostname}`,
+    null
+  );
 
   watch(
-    [activeTheme, availableThemes],
-    async () => {
-      // if available theme is set to a theme that isn't available, set it to the default theme
-      if (!availableThemes.value.includes(activeTheme.value)) {
-        activeTheme.value = defaultTheme.value;
-      }
+    [activeTheme, instanceData],
+    async (...args) => {
+      console.log("activeTheme or instanceData changed, applying theme", {
+        activeTheme: activeTheme.value,
+        instanceData: instanceData.value,
+        args,
+      });
+
+      if (!instanceData.value) return;
+
+      activeTheme.value = activeTheme.value || defaultTheme.value;
 
       // set theme on the body
       document.documentElement.setAttribute("data-theme", activeTheme.value);
