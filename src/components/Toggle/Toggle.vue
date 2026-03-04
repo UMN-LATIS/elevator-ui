4e3bb03fae1a4fc5a2519b9255553ed0f3d27b87
James Johnson <johnsojr@umn.edu>
2026-03-03 18:22:41 -0600

    test fixes

--- src/components/Toggle/Toggle.vue
+++ src/components/Toggle/Toggle.vue
@@ -3,9 +3,9 @@
     <span v-if="offLabel" class="text-sm">{{ offLabel }}</span>
     <SwitchRoot
       :modelValue="modelValue"
+      :aria-label="settingLabel"
       class="relative inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full p-[3px] shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-1 data-[state=checked]:bg-primary data-[state=unchecked]:bg-surface-container-highest"
       @update:modelValue="(val) => $emit('update:modelValue', val)">
-      <span class="sr-only">{{ settingLabel }}</span>
       <SwitchThumb
         class="pointer-events-none block h-3.5 w-3.5 rounded-full shadow ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=checked]:bg-on-primary data-[state=unchecked]:translate-x-0 data-[state=unchecked]:bg-outline" />
     </SwitchRoot>
