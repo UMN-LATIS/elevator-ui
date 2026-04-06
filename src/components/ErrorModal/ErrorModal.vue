f9112e20216884dfa27a800b466cc6c13ad9ace8
James Johnson <johnsojr@umn.edu>
2026-04-06 17:14:29 -0500

    refactor(auth): remove accidental reorder in ErrorModal

--- src/components/ErrorModal/ErrorModal.vue
+++ src/components/ErrorModal/ErrorModal.vue
@@ -30,9 +30,6 @@ import SignInRequiredNotice from "@/pages/HomePage/SignInRequiredNotice.vue";
 const errorStore = useErrorStore();
 
 const error = computed(() => errorStore.error);
-const isCurrentUserUnauthenticated = computed(() => {
-  return error.value instanceof ApiError && error.value.statusCode === 401;
-});
 const errorTitle = computed(() => {
   if (!(error.value instanceof ApiError)) {
     return "Error";
@@ -43,6 +40,9 @@ const errorTitle = computed(() => {
   }
 
   return `Error: ${error.value.statusCode}`;
+});
+const isCurrentUserUnauthenticated = computed(() => {
+  return error.value instanceof ApiError && error.value.statusCode === 401;
 });
 
 const messages: Record<number | string, string> = {
