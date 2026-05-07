ae855183014d6615df0daf05800e89d83151e4dd
James Johnson <johnsojr@umn.edu>
2026-05-07 14:28:48 -0500

    don't need this for hotfix

--- mock-server/db/assets.ts
+++ mock-server/db/assets.ts
@@ -113,100 +113,7 @@ const generateMockAssets = (count = 100): WithMeta<Asset>[] => {
   return assets;
 };
 
-// Assets with location + date data for map/timeline/drawer share testing
-const locationAssetSeeds: WithMeta<Asset>[] = [
-  {
-    ...baseAsset,
-    title_1: [
-      { isPrimary: true, fieldContents: "Minneapolis Institute of Arts" },
-    ],
-    assetId: "location_asset_minneapolis",
-    firstFileHandlerId: "handler_location_mpls",
-    title: ["Minneapolis Institute of Arts"],
-    location_1: [
-      {
-        isPrimary: true,
-        locationLabel: "Minneapolis",
-        address: "2400 3rd Ave S, Minneapolis, MN 55404",
-        loc: { type: "Point", coordinates: [-93.2733, 44.9584] },
-      },
-    ],
-    date_1: [
-      {
-        isPrimary: true,
-        label: "",
-        start: { text: "1883", numeric: "-2745964800" },
-        end: { text: "", numeric: null },
-      },
-    ],
-    modified: {
-      date: "2021-01-01 00:00:00.000000",
-      timezone_type: 3,
-      timezone: "UTC",
-    },
-  },
-  {
-    ...baseAsset,
-    title_1: [{ isPrimary: true, fieldContents: "Art Institute of Chicago" }],
-    assetId: "location_asset_chicago",
-    firstFileHandlerId: "handler_location_chi",
-    title: ["Art Institute of Chicago"],
-    location_1: [
-      {
-        isPrimary: true,
-        locationLabel: "Chicago",
-        address: "111 S Michigan Ave, Chicago, IL 60603",
-        loc: { type: "Point", coordinates: [-87.6237, 41.8796] },
-      },
-    ],
-    date_1: [
-      {
-        isPrimary: true,
-        label: "",
-        start: { text: "1879", numeric: "-2871964800" },
-        end: { text: "", numeric: null },
-      },
-    ],
-    modified: {
-      date: "2022-06-15 00:00:00.000000",
-      timezone_type: 3,
-      timezone: "UTC",
-    },
-  },
-  {
-    ...baseAsset,
-    title_1: [{ isPrimary: true, fieldContents: "SFMOMA" }],
-    assetId: "location_asset_sf",
-    firstFileHandlerId: "handler_location_sf",
-    title: ["SFMOMA"],
-    location_1: [
-      {
-        isPrimary: true,
-        locationLabel: "San Francisco",
-        address: "151 3rd St, San Francisco, CA 94103",
-        loc: { type: "Point", coordinates: [-122.4009, 37.7857] },
-      },
-    ],
-    date_1: [
-      {
-        isPrimary: true,
-        label: "",
-        start: { text: "1935", numeric: "-1104537600" },
-        end: { text: "", numeric: null },
-      },
-    ],
-    modified: {
-      date: "2023-03-20 00:00:00.000000",
-      timezone_type: 3,
-      timezone: "UTC",
-    },
-  },
-];
-
-export const LOCATION_ASSET_IDS = locationAssetSeeds.map((a) => a.assetId);
-
 const assetSeeds: WithMeta<Asset>[] = [
-  ...locationAssetSeeds,
   baseAsset,
   // Asset with broken template for error boundary testing
   {
@@ -278,36 +185,6 @@ const assetSeeds: WithMeta<Asset>[] = [
     collectionId: 1,
     modifiedBy: 1,
   },
-  // Asset with both clickable and non-clickable tags for contrast testing
-  {
-    ...baseAsset,
-    title_1: [
-      { isPrimary: true, fieldContents: "Tag Contrast Test Asset" },
-    ],
-    sometags_1: [
-      {
-        isPrimary: false,
-        tags: ["archival", "photograph", "19th century"],
-      },
-    ],
-    clickabletags_1: [
-      {
-        isPrimary: false,
-        tags: ["Minneapolis", "portrait", "daguerreotype"],
-      },
-    ],
-    assetId: "tag_contrast_test_001",
-    firstFileHandlerId: "handler_tag_contrast_001",
-    title: ["Tag Contrast Test Asset"],
-    templateId: 68,
-    collectionId: 1,
-    modifiedBy: 1,
-    modified: {
-      date: "2026-04-01 12:00:00.000000",
-      timezone_type: 3,
-      timezone: "UTC",
-    },
-  },
   // Auth-required asset for testing 401 / login-redirect flows
   {
     ...baseAsset,
@@ -324,64 +201,6 @@ const assetSeeds: WithMeta<Asset>[] = [
       timezone: "UTC",
     },
     _meta: { visibility: "authenticated" },
-  },
-  // Soft-deleted assets for trash tab testing
-  {
-    ...baseAsset,
-    title_1: [{ isPrimary: false, fieldContents: "Deleted Photo Essay" }],
-    assetId: "deleted_asset_001",
-    firstFileHandlerId: "handler_deleted_001",
-    title: ["Deleted Photo Essay"],
-    templateId: 1,
-    collectionId: 1,
-    createdBy: 1,
-    modifiedBy: 1,
-    deleted: true,
-    deletedBy: 1,
-    deletedAt: "2026-03-15T10:30:00.000Z",
-    modified: {
-      date: "2026-03-10 08:00:00.000000",
-      timezone_type: 3,
-      timezone: "UTC",
-    },
-  },
-  {
-    ...baseAsset,
-    title_1: [{ isPrimary: false, fieldContents: "Deleted Audio Recording" }],
-    assetId: "deleted_asset_002",
-    firstFileHandlerId: "handler_deleted_002",
-    title: ["Deleted Audio Recording"],
-    templateId: 1,
-    collectionId: 2,
-    createdBy: 1,
-    modifiedBy: 1,
-    deleted: true,
-    deletedBy: 1,
-    deletedAt: "2026-03-16T14:45:00.000Z",
-    modified: {
-      date: "2026-03-12 12:00:00.000000",
-      timezone_type: 3,
-      timezone: "UTC",
-    },
-  },
-  {
-    ...baseAsset,
-    title_1: [{ isPrimary: false, fieldContents: "Deleted Video Clip" }],
-    assetId: "deleted_asset_003",
-    firstFileHandlerId: "handler_deleted_003",
-    title: ["Deleted Video Clip"],
-    templateId: 1,
-    collectionId: 1,
-    createdBy: 2,
-    modifiedBy: 2,
-    deleted: true,
-    deletedBy: 2,
-    deletedAt: "2026-03-17T09:15:00.000Z",
-    modified: {
-      date: "2026-03-14 16:30:00.000000",
-      timezone_type: 3,
-      timezone: "UTC",
-    },
   },
   ...generateMockAssets(),
 ];
