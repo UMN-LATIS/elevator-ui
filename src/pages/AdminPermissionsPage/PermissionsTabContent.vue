da65252eb3fb4a4181138c19f68834baa962260f
James Johnson <johnsojr@umn.edu>
2026-07-07 11:55:46 -0500

    stub permissions tab content

---
+++ src/pages/AdminPermissionsPage/PermissionsTabContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p class="text-sm">
+      Grant groups access to all collections or a single collection.
+    </p>
+    <div
+      class="mt-4 rounded-md border border-dashed border-outline-variant p-10 text-center text-sm text-on-surface-variant">
+      Permissions table (Not Implemented Yet)
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
