5ff397b9fd420afbdebdbf1b6554fea251fa92ce
James Johnson <johnsojr@umn.edu>
2026-07-24 15:52:15 -0500

    fix

--- src/components/Widget/UploadWidget/UploadWidget.vue
+++ src/components/Widget/UploadWidget/UploadWidget.vue
@@ -47,7 +47,7 @@
         v-for="(content, key) in contents"
         :key="key"
         class="max-w-sm"
-        :tip="descriptionTip(content)">
+        :tip="isDescriptionVisible ? descriptionText(content) : ''">
         <button
           class="thumbnail-related-asset-widget flex flex-col rounded-md border border-transparent p-1 no-underline hover:no-underline w-24"
           :class="{
@@ -61,7 +61,7 @@
             See https://www.w3.org/WAI/tutorials/images/functional/ example 2 -->
           <ThumbnailImage
             :src="`${config.instance.base.url}/fileManager/tinyImageByFileId/${content.fileId}/true`"
-            :alt="isDescriptionVisible ? '' : content.fileDescription"
+            :alt="isDescriptionVisible ? '' : descriptionText(content)"
             :fileType="content.fileType"
             class="thumbnail-related-asset-widget__image max-w-full" />
           <SanitizedHTML
@@ -112,8 +112,7 @@ const isDescriptionVisible = computed(
   () => instanceStore.instance.showThumbnailDescription
 );
 
-function descriptionTip(content: UploadWidgetContent): string {
-  if (!isDescriptionVisible.value) return "";
+function descriptionText(content: UploadWidgetContent): string {
   return content.fileDescription ?? "";
 }
 
