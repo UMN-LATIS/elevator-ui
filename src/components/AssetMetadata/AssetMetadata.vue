3c71637c51c1b51f4c915ca6c21a8ade62e12281
James Johnson <johnsojr@umn.edu>
2026-08-18 09:24:24 -0500

    fix gap between metadata items

--- src/components/AssetMetadata/AssetMetadata.vue
+++ src/components/AssetMetadata/AssetMetadata.vue
@@ -1,6 +1,5 @@
 <template>
-  <div
-    class="asset-metadata flex flex-col gap-[var(--app-panel-body-items-gap)]">
+  <div class="asset-metadata flex flex-col gap-4">
     <template v-if="assetId && asset && template">
       <CollectionTuple
         v-if="showCollectionTop"
